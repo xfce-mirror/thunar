2e712c264ad0d8bdb2d1d95088de195fbf2fbf92
Benedikt Meurer <benny@xfce.org>
2005-09-02 21:19:47 +0000

    2005-09-02	Benedikt Meurer <benny@xfce.org>
    	* thunar/thunar-icon-renderer.c(thunar_icon_renderer_render): Don't
    	  scale the icon if it fits into the cell area.
    
    
    
    
    (Old svn revision: 17295)

--- thunar/thunar-icon-renderer.c
+++ thunar/thunar-icon-renderer.c
@@ -352,7 +352,7 @@ thunar_icon_renderer_render (GtkCellRenderer     *renderer,
   icon_area.height = gdk_pixbuf_get_height (icon);
 
   /* scale down the icon on-demand */
-  if (G_UNLIKELY (icon_area.width > icon_renderer->size || icon_area.height > icon_renderer->size))
+  if (G_UNLIKELY (icon_area.width > cell_area->width || icon_area.height > cell_area->height))
     {
       temp = exo_gdk_pixbuf_scale_ratio (icon, icon_renderer->size);
       g_object_unref (G_OBJECT (icon));
