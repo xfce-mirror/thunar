39455c32dd513dfd7ff448add610751557f11a37
Benedikt Meurer <benny@xfce.org>
2006-01-30 23:19:04 +0000

    2006-01-31	Benedikt Meurer <benny@xfce.org>
    	* thunar/thunar-standard-view.c(thunar_standard_view_scroll_event): Add
    	  support to zoom in/out using Control+MouseWheel.
    
    
    
    
    (Old svn revision: 19679)

--- thunar/thunar-standard-view.c
+++ thunar/thunar-standard-view.c
@@ -2123,6 +2123,16 @@ thunar_standard_view_scroll_event (GtkWidget          *view,
         }
     }
 
+  /* zoom-in/zoom-out on control+mouse wheel */
+  if ((event->state & GDK_CONTROL_MASK) != 0 && (event->direction == GDK_SCROLL_UP || event->direction == GDK_SCROLL_DOWN))
+    {
+      thunar_view_set_zoom_level (THUNAR_VIEW (standard_view),
+          (event->direction == GDK_SCROLL_UP)
+          ? MIN (standard_view->priv->zoom_level + 1, THUNAR_ZOOM_N_LEVELS - 1)
+          : MAX (standard_view->priv->zoom_level, 1) - 1);
+      return TRUE;
+    }
+
   /* next please... */
   return FALSE;
 }
