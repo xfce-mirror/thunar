9e1abba48d759234cd63e52923781de0151a6b17
Benedikt Meurer <benny@xfce.org>
2006-03-06 15:43:06 +0000

    2006-03-06	Benedikt Meurer <benny@xfce.org>
    	* autogen.sh, configure.in.in, po/LINGUAS: Read the set of available
    	  languages from po/LINGUAS and substitute them when autogen.sh is
    	  run. This way we can continue to use glib-gettext.
    	* docs/manual/, po-doc/, configure.in.in, Makefile.am: Import Daichi's
    	  initial japanese translations for the user manual.
    
    
    
    
    (Old svn revision: 20256)

--- autogen.sh
+++ autogen.sh
@@ -18,9 +18,21 @@ EOF
   exit 1
 }
 
-# substitute revision and date
+# verify that po/LINGUAS is present
+(test -f po/LINGUAS) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
+            or try to checkout again.
+EOF
+  exit 1
+}
+
+# substitute revision and linguas
+linguas=`sed -e '/^#/d' po/LINGUAS`
 revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
-sed -e "s/@REVISION@/${revision}/g" < "configure.in.in" > "configure.in"
+sed -e "s/@LINGUAS@/${linguas}/g" \
+    -e "s/@REVISION@/${revision}/g" \
+    < "configure.in.in" > "configure.in"
 
 exec xdt-autogen $@
 
