8a9a5174448ff5699f6935c04b3c86642634fb92
Jannis Pohlmann <jannis@xfce.org>
2010-12-06 10:34:51 +0100

    Hide status icon prior to destroying it (bug #6891). Patch by Xavier D.
    This appears to be a bug in GTK+. If the status icon is not hidden
    before it is destroyed, it will sometimes not disappear (the
    notification area will never receive a "removed" signal). For more
    information please see
    
      http://bugzilla.gnome.org/show_bug.cgi?id=589860

--- thunar/thunar-progress-dialog.c
+++ thunar/thunar-progress-dialog.c
@@ -1,6 +1,6 @@
 /* vi:set et ai sw=2 sts=2 ts=2: */
 /*-
- * Copyright (c) 2009 Jannis Pohlmann <jannis@xfce.org>
+ * Copyright (c) 2009-2010 Jannis Pohlmann <jannis@xfce.org>
  *
  * This program is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU General Public License as
@@ -131,7 +131,10 @@ thunar_progress_dialog_finalize (GObject *object)
 
   /* destroy the status icon */
   if (dialog->status_icon != NULL)
-    g_object_unref (dialog->status_icon);
+    {
+      gtk_status_icon_set_visible (dialog->status_icon, FALSE);
+      g_object_unref (dialog->status_icon);
+    }
 
   /* free the view list */
   g_list_free (dialog->views);
