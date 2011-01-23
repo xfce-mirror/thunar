3f57e2b554f0d7ff0122d5e12968be1768b3f950
Jannis Pohlmann <jannis@xfce.org>
2011-01-23 14:05:31 +0100

    Paste files in the correct order (bug #6504).

--- thunar/thunar-clipboard-manager.c
+++ thunar/thunar-clipboard-manager.c
@@ -1,21 +1,22 @@
-/* $Id$ */
+/* vi:set et ai sw=2 sts=2 ts=2: */
 /*-
  * Copyright (c) 2005-2006 Benedikt Meurer <benny@xfce.org>
- * Copyright (c) 2009 Jannis Pohlmann <jannis@xfce.org>
+ * Copyright (c) 2009-2011 Jannis Pohlmann <jannis@xfce.org>
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public 
+ * License along with this program; if not, write to the Free 
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -467,7 +468,7 @@ thunar_clipboard_manager_transfer_files (ThunarClipboardManager *manager,
   manager->files_cutted = !copy;
 
   /* setup the new file list */
-  for (lp = files, manager->files = NULL; lp != NULL; lp = lp->next)
+  for (lp = g_list_last (files), manager->files = NULL; lp != NULL; lp = lp->prev)
     {
       file = g_object_ref (G_OBJECT (lp->data));
       manager->files = g_list_prepend (manager->files, file);
