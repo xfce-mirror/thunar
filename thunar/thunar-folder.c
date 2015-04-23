ea00375c7d041105033678d5515b651a34e1804d
Harald Judt <h.judt@gmx.at>
2015-04-23 21:41:45 +0200

    Do not perform the complete folder reload in idle (bug #11849)
    This can cause an infinite loop as described in the bug report.

--- thunar/thunar-folder.c
+++ thunar/thunar-folder.c
@@ -551,10 +551,10 @@ thunar_folder_finished (ExoJob       *job,
   if (folder->reload_info)
     {
       for (lp = folder->files; lp != NULL; lp = lp->next)
-        thunar_file_reload_idle (lp->data);
+        thunar_file_reload (lp->data);
 
       /* reload folder information too */
-      thunar_file_reload_idle (folder->corresponding_file);
+      thunar_file_reload (folder->corresponding_file);
 
       folder->reload_info = FALSE;
     }
