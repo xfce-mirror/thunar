e859b82dfef46eefb3af95f5b632222526c9293e
Alexander Schwinn <acs82@gmx.de>
2018-06-06 23:07:48 +0200

    Disable thumbnail frame for the 1.8.0 release, since it may have an performance impact. (Bug #14433)

--- thunar/thunar-icon-factory.c
+++ thunar/thunar-icon-factory.c
@@ -443,8 +443,11 @@ thunar_icon_factory_load_from_file (ThunarIconFactory *factory,
       /* check if we want to add a frame to the image (we really don't
        * want to do this for icons displayed in the details view).
        */
-      needs_frame = (strstr (path, G_DIR_SEPARATOR_S ".cache/thumbnails" G_DIR_SEPARATOR_S) != NULL)
-                 && (size >= 32) && thumbnail_needs_frame (pixbuf, width, height);
+      needs_frame = FALSE;
+      /* Disabled for 1.8.0 release, will be made optional later
+       * needs_frame = (strstr (path, G_DIR_SEPARATOR_S ".cache/thumbnails" G_DIR_SEPARATOR_S) != NULL)
+       *         && (size >= 32) && thumbnail_needs_frame (pixbuf, width, height);
+       */
 
       /* be sure to make framed thumbnails fit into the size */
       if (G_LIKELY (needs_frame))
