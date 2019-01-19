b1b693cf122a1a95c7a2b497ccdef49b6a8bc81a
Roy Richardson <rocketx86@gmail.com>
2019-01-19 22:53:14 +0100

    FreeBSD -Thunar crash due to passing null to strcmp() (Bug #15059)

--- thunar/thunar-file.c
+++ thunar/thunar-file.c
@@ -4096,11 +4096,11 @@ thunar_file_compare_by_name (const ThunarFile *file_a,
 
   /* case insensitive checking */
   if (G_LIKELY (!case_sensitive))
-    result = strcmp (file_a->collate_key_nocase, file_b->collate_key_nocase);
+    result = g_strcmp0 (file_a->collate_key_nocase, file_b->collate_key_nocase);
 
   /* fall-back to case sensitive */
   if (result == 0)
-    result = strcmp (file_a->collate_key, file_b->collate_key);
+    result = g_strcmp0 (file_a->collate_key, file_b->collate_key);
 
   /* this happens in the trash */
   if (result == 0)
