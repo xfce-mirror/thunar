6e84219f0879d7f5024702ef7fdcbdefc34d60bb
Andre Miranda <andreldm@xfce.org>
2019-06-16 18:04:18 -0300

    Rename (Bug #15579)

--- plugins/thunar-sbr/thunar-sbr-enum-types.h
+++ plugins/thunar-sbr/thunar-sbr-enum-types.h
@@ -31,7 +31,7 @@ G_BEGIN_DECLS;
  * ThunarSbrCaseRenamerMode:
  * @THUNAR_SBR_CASE_RENAMER_MODE_LOWER       : convert to lower case.
  * @THUNAR_SBR_CASE_RENAMER_MODE_UPPER       : convert to upper case.
- * @THUNAR_SBR_CASE_RENAMER_MODE_CAMEL       : convert to camel case.
+ * @THUNAR_SBR_CASE_RENAMER_MODE_TITLE       : convert to title case.
  * @THUNAR_SBR_CASE_RENAMER_MODE_FIRST_UPPER : convert to First character uppercase.
  *
  * The operation mode for the #ThunarSbrCaseRenamer.
@@ -40,7 +40,7 @@ typedef enum
 {
   THUNAR_SBR_CASE_RENAMER_MODE_LOWER,
   THUNAR_SBR_CASE_RENAMER_MODE_UPPER,
-  THUNAR_SBR_CASE_RENAMER_MODE_CAMEL,
+  THUNAR_SBR_CASE_RENAMER_MODE_TITLE,
   THUNAR_SBR_CASE_RENAMER_MODE_FIRST_UPPER,
 } ThunarSbrCaseRenamerMode;
 
