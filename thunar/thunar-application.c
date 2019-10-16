a87c890d78d749cc69125bf744913d9ea6576617
Yury Martynov <email@linxon.ru>
2019-10-16 14:46:30 +0300

    Not possible to grab scrollbar on the very right pixels when Thunar is maximized (Bug #16050)

--- thunar/thunar-application.c
+++ thunar/thunar-application.c
@@ -593,7 +593,7 @@ thunar_application_load_css (void)
     /* add missing top border to side pane */
     ".shortcuts-pane { border-top-style: solid; }"
     /* make border thicker during DnD */
-    ".standard-view { border-left-width: 0px; }"
+    ".standard-view { border-left-width: 0px; border-right-width: 0px; }"
     ".standard-view:drop(active) { border-width: 2px; }", -1, NULL);
   screen = gdk_screen_get_default ();
   gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (css_provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
