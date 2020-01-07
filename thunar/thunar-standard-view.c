39cc89f5a94428a4f8b76bccb4ddae83fb958020
Sébastien Marie <semarie@online.fr>
2020-01-07 06:21:46 +0100

    NULL is the proper sentinel for g_object_new() (Bug #16310)
    Avoid SIGSEGV on OpenBSD and NetBSD.

--- thunar/thunar-standard-view.c
+++ thunar/thunar-standard-view.c
@@ -719,7 +719,7 @@ G_GNUC_END_IGNORE_DEPRECATIONS
 #endif
                                                "alignment", PANGO_ALIGN_CENTER,
                                                "xalign", 0.5,
-                                               FALSE);
+                                               NULL);
   g_object_ref_sink (G_OBJECT (standard_view->name_renderer));
 
   /* TODO: prelit underline
