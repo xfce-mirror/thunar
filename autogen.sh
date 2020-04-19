047ebe15f30a62236cdef0f6b4d796f19cf3285a
Romain Bouvier <skunnyk@alteroot.org>
2020-04-19 17:29:57 +0200

    Bug #16717: Move from exo-csource to xdt-csource
    - exo-csource is deprecated and moved to xdt-csource
    - Bump minimal xdt version

--- autogen.sh
+++ autogen.sh
@@ -24,11 +24,11 @@
 autogen.sh: You don't seem to have the Xfce development tools installed on
             your system, which are required to build this software.
             Please install the xfce4-dev-tools package first, it is available
-            from https://www.xfce.org/.
+            from your distribution or https://www.xfce.org/.
 EOF
   exit 1
 }
 
 test -d m4 || mkdir m4
 
-XDT_AUTOGEN_REQUIRED_VERSION="4.7.2" exec xdt-autogen $@
+XDT_AUTOGEN_REQUIRED_VERSION="4.14.0" exec xdt-autogen $@
