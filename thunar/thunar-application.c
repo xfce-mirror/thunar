3882e850298377b6e2a99cced5a7c2a8e8ac25d4
Reuben Green <reubengreen73@gmail.com>
2020-06-20 10:58:48 +0100

    Implements per directory view type (#8)
    Allows the view type (details, icon, compact) to be saved individually for each directory.
    This requires some significant changes to the way thunar-window handles changing directories
    and creating new views. Also adds an option in the view menu to delete all saved view settings
    for the current directory.

--- thunar/thunar-application.c
+++ thunar/thunar-application.c
@@ -1316,7 +1316,7 @@ thunar_application_open_window (ThunarApplication *application,
           list = g_list_last (list);
 
           if (directory != NULL)
-              thunar_window_notebook_insert (THUNAR_WINDOW (list->data), directory);
+              thunar_window_notebook_open_new_tab (THUNAR_WINDOW (list->data), directory);
           
           /* bring the window to front */
           gtk_window_present (list->data);
