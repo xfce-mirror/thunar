47f5c11d63aaa87c71657d333bd8e7cd8a383b88
Alexander Schwinn <alexxcons@xfce.org>
2021-01-07 22:10:58 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- thunar/thunar-window.c
+++ thunar/thunar-window.c
@@ -2152,7 +2152,6 @@ thunar_window_paned_notebooks_switch (ThunarWindow *window)
 
   if (window->notebook_selected == window->notebook_left)
     new_curr_notebook = window->notebook_right;
-    } 
   else if (window->notebook_selected == window->notebook_right)
     {
       new_curr_notebook = window->notebook_left;
