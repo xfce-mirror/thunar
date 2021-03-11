xfce-mirror/thunar

commit 7675b9ea4daa3ece93c747cd109a61760aebcfb1

thunar-launcher: Unify way to set selected device/location/files
In order to minimize risk of memory leaks

thunar/thunar-launcher.c
@@ -193,6 +193,10 @@ static void                    thunar_launcher_action_create_folder       (Thuna
 static void                    thunar_launcher_action_create_document     (ThunarLauncher                 *launcher,
                                                                            GtkWidget                      *menu_item);
 static GtkWidget              *thunar_launcher_create_document_submenu_new(ThunarLauncher                 *launcher);
+void                           thunar_launcher_set_selection              (ThunarLauncher                 *launcher,
+                                                                           GList                          *selected_thunar_files,
+                                                                           ThunarDevice                   *selected_device,
+                                                                           GFile                          *selected_location);
 
 
 
@@ -487,7 +491,7 @@ thunar_launcher_set_property (GObject      *object,
       break;
 
     case PROP_SELECTED_FILES:
-      thunar_component_set_selected_files (THUNAR_COMPONENT (object), g_value_get_boxed (value));
+      thunar_launcher_set_selection (launcher, g_value_get_boxed (value), NULL, NULL);
       break;
 
     case PROP_WIDGET:
@@ -499,12 +503,12 @@ thunar_launcher_set_property (GObject      *object,
       break;
 
     case PROP_SELECTED_DEVICE:
-      launcher->device_to_process = g_value_get_pointer (value);
+      thunar_launcher_set_selection (launcher, NULL, g_value_get_boxed (value), NULL);
       break;
 
     case PROP_SELECTED_LOCATION:
-      launcher->location_to_process = g_value_get_pointer (value);
-      break;
+      thunar_launcher_set_selection (launcher, NULL, NULL, g_value_get_boxed (value));
+     break;
 
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -2717,8 +2721,6 @@ thunar_launcher_action_eject_finish (ThunarDevice  *device,
       thunar_dialogs_show_error (GTK_WIDGET (launcher->widget), error, _("Failed to eject \"%s\""), device_name);
       g_free (device_name);
     }
-  else
-    launcher->device_to_process = NULL;
 
   g_object_unref (launcher);
 }
@@ -2775,8 +2777,6 @@ thunar_launcher_action_unmount_finish (ThunarDevice *device,
       thunar_dialogs_show_error (GTK_WIDGET (launcher->widget), error, _("Failed to unmount \"%s\""), device_name);
       g_free (device_name);
     }
-  else
-    launcher->device_to_process = NULL;
 
   g_object_unref (launcher);
 }
@@ -2967,4 +2967,40 @@ thunar_launcher_get_widget (ThunarLauncher *launcher)
 {
   _thunar_return_val_if_fail (THUNAR_IS_LAUNCHER (launcher), NULL);
   return launcher->widget;
+}
+
+
+
+/**
+ * thunar_launcher_set_selection:
+ * @launcher : a #ThunarLauncher instance
+ * @selected_thunar_files: #GList of selected #ThunarFile instances, or NULL
+ * @selected_device: selected #ThunarDevice or NULL
+ * @selected_location: selected #GFile (possibly only holds an URI), or NULL
+ *
+ * Will set the related items as "selection" and clear any previous selection.
+ * Note that always only one of the 3 "selected" arguments should be set.
+ **/
+void
+thunar_launcher_set_selection (ThunarLauncher *launcher,
+                               GList          *selected_thunar_files,
+                               ThunarDevice   *selected_device,
+                               GFile          *selected_location)
+{
+  _thunar_return_if_fail (THUNAR_IS_LAUNCHER (launcher));
+
+  /* unref the current device/location */
+  if (launcher->device_to_process != NULL)
+    g_object_unref (launcher->device_to_process);
+  if (launcher->location_to_process != NULL)
+    g_object_unref (launcher->location_to_process);
+
+  /* ref the new device/location */
+  if (selected_device != NULL)
+    launcher->device_to_process = g_object_ref (selected_device);
+  if (selected_location != NULL)
+    launcher->location_to_process = g_object_ref (selected_location);
+
+  /* for selected files things are a bit more conmplicated */
+  thunar_launcher_set_selected_files (THUNAR_COMPONENT (launcher), selected_thunar_files);
 }

thunar/thunar-tree-view.c
@@ -853,7 +853,6 @@ thunar_tree_view_key_press_event(GtkWidget   *widget,
                   /* mark this path for selection after unmounting */
                   view->select_path = gtk_tree_path_copy(path);
                   g_object_set (G_OBJECT (view->launcher), "selected-device", device, NULL);
-                  g_object_set (G_OBJECT (view->launcher), "selected-files", NULL, "current-directory", NULL, NULL);
                   thunar_launcher_action_unmount (view->launcher);
                   g_object_unref (G_OBJECT (device));
                 }
@@ -872,7 +871,6 @@ thunar_tree_view_key_press_event(GtkWidget   *widget,
      if (device != NULL && thunar_device_is_mounted (device) == FALSE)
        {
          g_object_set (G_OBJECT (view->launcher), "selected-device", device, NULL);
-         g_object_set (G_OBJECT (view->launcher), "selected-files", NULL, "current-directory", NULL, NULL);
          thunar_launcher_action_mount (view->launcher);
        }
      else
@@ -1155,7 +1153,6 @@ thunar_tree_view_test_expand_row (GtkTreeView *tree_view,
           /* we need to mount the device before we can expand the row */
           expandable = FALSE;
           g_object_set (G_OBJECT (view->launcher), "selected-device", device, NULL);
-          g_object_set (G_OBJECT (view->launcher), "selected-files", NULL, "current-directory", NULL, NULL);
           /* The closure will expand the row after the mount operation finished */
           thunar_launcher_action_mount (view->launcher);
         }
@@ -1238,8 +1235,6 @@ thunar_tree_view_context_menu (ThunarTreeView *view,
                                                  "launcher", view->launcher,
                                                  "force-section-open", TRUE, NULL);
 
-  g_object_set (G_OBJECT (view->launcher), "selected-device", device, NULL);
-
   file_is_available = (device == NULL || thunar_device_is_mounted (device));
   if (file_is_available)
     {
@@ -1272,7 +1267,7 @@ thunar_tree_view_context_menu (ThunarTreeView *view,
     }
   else
     {
-      g_object_set (G_OBJECT (view->launcher), "selected-files", NULL, "current-directory", NULL, NULL);
+      g_object_set (G_OBJECT (view->launcher), "selected-device", device, NULL);
       thunar_launcher_append_menu_item (view->launcher, GTK_MENU_SHELL (context_menu), THUNAR_LAUNCHER_ACTION_OPEN, TRUE);
       thunar_launcher_append_menu_item (view->launcher, GTK_MENU_SHELL (context_menu), THUNAR_LAUNCHER_ACTION_OPEN_IN_TAB, TRUE);
       thunar_launcher_append_menu_item (view->launcher, GTK_MENU_SHELL (context_menu), THUNAR_LAUNCHER_ACTION_OPEN_IN_WINDOW, TRUE);
@@ -1484,7 +1479,6 @@ thunar_tree_view_action_open (ThunarTreeView *view)
       else
       {
         g_object_set (G_OBJECT (view->launcher), "selected-device", device, NULL);
-        g_object_set (G_OBJECT (view->launcher), "selected-files", NULL, "current-directory", NULL, NULL);
         thunar_launcher_action_mount (view->launcher);
       }
     }

thunar/thunar-window.c
@@ -3542,8 +3542,7 @@ thunar_window_action_open_bookmark (GFile *g_file)
 
   window = g_object_get_data (G_OBJECT (g_file), I_("thunar-window"));
 
-  g_object_set (G_OBJECT (window->launcher), "selected-location", g_file,
-                                             "selected-files", NULL, NULL);
+  g_object_set (G_OBJECT (window->launcher), "selected-location", g_file, NULL);
   thunar_launcher_activate_selected_files (window->launcher, THUNAR_LAUNCHER_CHANGE_DIRECTORY, NULL);
 }
 

commit 13cd8db63f953d226d7ff5ce2ec65c020098923f

Regression: Missing "mount"/"unmount" on tree-view devices
Regression introduced by 7675b9ea4daa3ece93c747cd109a61760aebcfb1 (only
on master, was not released)

For devices it is required to provide as well a ThunarFile to the
launcher. The 'ThunarFile' is needed to display UCA's, the
'ThunarDevice' is needed for mount/unmount menu-items.

thunar/thunar-launcher.c
@@ -194,10 +194,6 @@ static void                    thunar_launcher_action_create_folder       (Thuna
 static void                    thunar_launcher_action_create_document     (ThunarLauncher                 *launcher,
                                                                            GtkWidget                      *menu_item);
 static GtkWidget              *thunar_launcher_create_document_submenu_new(ThunarLauncher                 *launcher);
-void                           thunar_launcher_set_selection              (ThunarLauncher                 *launcher,
-                                                                           GList                          *selected_thunar_files,
-                                                                           ThunarDevice                   *selected_device,
-                                                                           GFile                          *selected_location);
 
 
 
@@ -212,8 +208,6 @@ struct _ThunarLauncher
 
   ThunarFile             *current_directory;
 
-  /* Note always only one of the three 'to_process' variables will be set ! */
-  /* The other two will be NULL. */
   GList                  *files_to_process;    /* List of thunar-files to work with */
   ThunarDevice           *device_to_process;   /* Device to work with */
   GFile			 *location_to_process; /* Location to work with (might be not reachable) */

thunar/thunar-launcher.h
@@ -22,6 +22,7 @@
 #define __THUNAR_LAUNCHER_H__
 
 #include <thunar/thunar-component.h>
+#include <thunar/thunar-device.h>
 
 G_BEGIN_DECLS;
 
@@ -101,6 +102,10 @@ gboolean        thunar_launcher_check_uca_key_activation             (ThunarLaun
 void            thunar_launcher_action_mount                         (ThunarLauncher                 *launcher);
 void            thunar_launcher_action_unmount                       (ThunarLauncher                 *launcher);
 void            thunar_launcher_action_eject                         (ThunarLauncher                 *launcher);
+void            thunar_launcher_set_selection                        (ThunarLauncher                 *launcher,
+                                                                      GList                          *selected_thunar_files,
+                                                                      ThunarDevice                   *selected_device,
+                                                                      GFile                          *selected_location);
 
 
 G_END_DECLS;

thunar/thunar-tree-view.c
@@ -1239,7 +1239,8 @@ thunar_tree_view_context_menu (ThunarTreeView *view,
   if (file_is_available)
     {
       files = g_list_append (NULL, file);
-      g_object_set (G_OBJECT (view->launcher), "selected-files", files, "current-directory", view->current_directory, NULL);
+      g_object_set (G_OBJECT (view->launcher), "current-directory", view->current_directory, NULL);
+      thunar_launcher_set_selection (view->launcher, files, device, NULL);
       g_list_free (files);
 
       if(thunar_g_file_is_trash    (thunar_file_get_file (file)) ||