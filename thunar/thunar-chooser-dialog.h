73c306fd8020bf7fa764d6653c552d4faafead65
Utsav <201901076@daiict.ac.in>
2021-03-22 22:58:41 +0530

    Add 'Set Default Program' feature into 'Open with' submenu

--- thunar/thunar-chooser-dialog.h
+++ thunar/thunar-chooser-dialog.h
@@ -41,6 +41,10 @@ void        thunar_show_chooser_dialog      (gpointer             parent,
                                              gboolean             open,
                                              gboolean             preselect_default_checkbox);
 
+void        thunar_show_set_default_program_chooser_dialog      (gpointer             parent,
+                                                                 ThunarFile          *file,
+                                                                 gboolean             open);
+
 G_END_DECLS;
 
 #endif /* !__THUNAR_CHOOSER_DIALOG_H__ */
