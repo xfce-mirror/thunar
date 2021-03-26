169394cd06233f9acf412c9f2860e001a3c5ce52
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2021-03-26 10:31:12 +0200

    Add `Empty Trash` button while in the trash folder (Issue #11)
    Issue #11
    MR !78

--- thunar/thunar-launcher.c
+++ thunar/thunar-launcher.c
@@ -185,7 +185,6 @@ static void                    thunar_launcher_action_restore             (Thuna
 static void                    thunar_launcher_action_move_to_trash       (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_delete              (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_trash_delete        (ThunarLauncher                 *launcher);
-static void                    thunar_launcher_action_empty_trash         (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_cut                 (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_copy                (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_paste               (ThunarLauncher                 *launcher);
@@ -2367,7 +2366,7 @@ thunar_launcher_action_trash_delete (ThunarLauncher *launcher)
 
 
 
-static void
+void
 thunar_launcher_action_empty_trash (ThunarLauncher *launcher)
 {
   ThunarApplication *application;
