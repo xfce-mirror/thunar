3b0878a4d9da8c427b879d0615702ee4bb02a059
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2021-04-02 01:59:57 +0300

    Add option for consistent horizontal spacing (Issue #118)
            Issue #118
            MR !86

--- thunar/thunar-icon-view.c
+++ thunar/thunar-icon-view.c
@@ -23,6 +23,7 @@
 
 #include <thunar/thunar-icon-view.h>
 #include <thunar/thunar-private.h>
+#include <thunar/thunar-preferences.h>
 
 
 
@@ -31,6 +32,7 @@ enum
 {
   PROP_0,
   PROP_TEXT_BESIDE_ICONS,
+  PROP_FIXED_ITEM_WIDTH
 };
 
 
@@ -52,6 +54,8 @@ struct _ThunarIconViewClass
 struct _ThunarIconView
 {
   ThunarAbstractIconView __parent__;
+  gboolean fixed_width;
+  gboolean text_beside_icons;
 };
 
 
@@ -89,6 +93,22 @@ thunar_icon_view_class_init (ThunarIconViewClass *klass)
                                                          "text-beside-icons",
                                                          FALSE,
                                                          EXO_PARAM_WRITABLE));
+
+  /**
+   * ThunarIconView:fixed-item-width:
+   *
+   * Whether the icon a view should always use the maximum width
+   * (= wrap width) for items displayed so that the grid layout
+   * stays the same between folders with either long or short file
+   * names
+   **/
+  g_object_class_install_property (gobject_class,
+                                   PROP_FIXED_ITEM_WIDTH,
+                                   g_param_spec_boolean ("fixed-item-width",
+                                                         "fixed-item-width",
+                                                         "fixed-item-width",
+                                                         TRUE,
+                                                         EXO_PARAM_WRITABLE));
 }
 
 
@@ -108,7 +128,10 @@ thunar_icon_view_init (ThunarIconView *icon_view)
 
   /* synchronize the "text-beside-icons" property with the global preference */
   exo_binding_new (G_OBJECT (THUNAR_STANDARD_VIEW (icon_view)->preferences), "misc-text-beside-icons", G_OBJECT (icon_view), "text-beside-icons");
-}
+  
+  /* synchronize the "fixed-item-width" property with the global preference */
+  exo_binding_new (G_OBJECT (THUNAR_STANDARD_VIEW (icon_view)->preferences), "misc-fixed-item-width", G_OBJECT (icon_view), "fixed-item-width");
+ }
 
 
 
@@ -119,12 +142,17 @@ thunar_icon_view_set_property (GObject      *object,
                                GParamSpec   *pspec)
 {
   ThunarStandardView *standard_view = THUNAR_STANDARD_VIEW (object);
+  ThunarIconView *icon_view = THUNAR_ICON_VIEW(standard_view);
 
   switch (prop_id)
     {
     case PROP_TEXT_BESIDE_ICONS:
       if (G_UNLIKELY (g_value_get_boolean (value)))
         {
+          icon_view->text_beside_icons = TRUE;
+          /* reset item width to automatic in case it has been modified via the fixed-width property */
+          exo_icon_view_set_item_width (EXO_ICON_VIEW (gtk_bin_get_child (GTK_BIN (standard_view))), -1);
+
           exo_icon_view_set_orientation (EXO_ICON_VIEW (gtk_bin_get_child (GTK_BIN (standard_view))), GTK_ORIENTATION_HORIZONTAL);
           g_object_set (G_OBJECT (standard_view->name_renderer), "wrap-width", 128, "yalign", 0.5f, "xalign", 0.0f, "alignment", PANGO_ALIGN_LEFT, NULL);
 
@@ -133,6 +161,8 @@ thunar_icon_view_set_property (GObject      *object,
         }
       else
         {
+          icon_view->text_beside_icons = FALSE;
+
           exo_icon_view_set_orientation (EXO_ICON_VIEW (gtk_bin_get_child (GTK_BIN (standard_view))), GTK_ORIENTATION_VERTICAL);
           g_object_set (G_OBJECT (standard_view->name_renderer), "yalign", 0.0f, "xalign", 0.5f, "alignment", PANGO_ALIGN_CENTER, NULL);
 
@@ -140,6 +170,16 @@ thunar_icon_view_set_property (GObject      *object,
           g_signal_connect (object, "notify::zoom-level", G_CALLBACK (thunar_icon_view_zoom_level_changed), NULL);
           thunar_icon_view_zoom_level_changed (standard_view);
         }
+      break;
+
+    case PROP_FIXED_ITEM_WIDTH:
+      icon_view->fixed_width = g_value_get_boolean (value);
+
+      /* only refresh item display and manipulate wrap-width when text is below icons (default) */
+      if (G_LIKELY (!icon_view->text_beside_icons))
+      {
+        thunar_icon_view_zoom_level_changed (standard_view);
+      }
       break;
 
     default:
@@ -175,6 +215,9 @@ static void
 thunar_icon_view_zoom_level_changed (ThunarStandardView *standard_view)
 {
   gint wrap_width;
+  gint max_width;
+
+  ThunarIconView *icon_view = THUNAR_ICON_VIEW (standard_view);
 
   _thunar_return_if_fail (THUNAR_IS_STANDARD_VIEW (standard_view));
 
@@ -183,29 +226,44 @@ thunar_icon_view_zoom_level_changed (ThunarStandardView *standard_view)
     {
     case THUNAR_ZOOM_LEVEL_25_PERCENT:
       wrap_width = 48;
+      max_width = 2;
       break;
 
     case THUNAR_ZOOM_LEVEL_38_PERCENT:
       wrap_width = 64;
+      max_width = 3;
       break;
 
     case THUNAR_ZOOM_LEVEL_50_PERCENT:
       wrap_width = 72;
+      max_width = 4;
       break;
 
     case THUNAR_ZOOM_LEVEL_75_PERCENT:
       wrap_width = 112;
+      max_width = 5;
+      break;
+
+    case THUNAR_ZOOM_LEVEL_100_PERCENT:
+      wrap_width = 128;
+      max_width = 8;
       break;
 
     default:
       wrap_width = 128;
+      max_width = 10;
       break;
     }
 
+  if (icon_view->fixed_width && !icon_view->text_beside_icons)
+  {
+    g_object_set (G_OBJECT (standard_view->name_renderer), "max-width-chars", max_width, NULL);
+  }
+  else
+  {
+    g_object_set (G_OBJECT (standard_view->name_renderer), "max-width-chars", -1, NULL);
+  }
+  
   /* set the new "wrap-width" for the text renderer */
   g_object_set (G_OBJECT (standard_view->name_renderer), "wrap-width", wrap_width, NULL);
 }
-
-
-
-
