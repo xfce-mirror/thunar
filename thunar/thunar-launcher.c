88279b86ba46ccb399b4fda95cda19481cc75fc7
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2021-04-02 00:18:00 +0300

    Add `Empty Trash` button while in the trash folder (Issue #11)
                Issue #11
                MR !78

--- thunar/thunar-launcher.c
+++ thunar/thunar-launcher.c
@@ -182,11 +182,9 @@ static void                    thunar_launcher_action_add_shortcuts       (Thuna
 static void                    thunar_launcher_action_make_link           (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_duplicate           (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_rename              (ThunarLauncher                 *launcher);
-static void                    thunar_launcher_action_restore             (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_move_to_trash       (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_delete              (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_trash_delete        (ThunarLauncher                 *launcher);
-static void                    thunar_launcher_action_empty_trash         (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_cut                 (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_copy                (ThunarLauncher                 *launcher);
 static void                    thunar_launcher_action_paste               (ThunarLauncher                 *launcher);
@@ -2322,7 +2320,7 @@ thunar_launcher_action_rename (ThunarLauncher *launcher)
 
 
 
-static void
+void
 thunar_launcher_action_restore (ThunarLauncher *launcher)
 {
   ThunarApplication *application;
@@ -2391,7 +2389,7 @@ thunar_launcher_action_trash_delete (ThunarLauncher *launcher)
 
 
 
-static void
+void
 thunar_launcher_action_empty_trash (ThunarLauncher *launcher)
 {
   ThunarApplication *application;
@@ -2400,7 +2398,7 @@ thunar_launcher_action_empty_trash (ThunarLauncher *launcher)
 
   if (launcher->single_directory_to_process == FALSE)
     return;
-  if (!thunar_file_is_root (launcher->single_folder) || !thunar_file_is_trashed (launcher->single_folder))
+  if (/*!thunar_file_is_root (launcher->single_folder) ||*/ !thunar_file_is_trashed (launcher->single_folder))
     return;
 
   application = thunar_application_get ();
