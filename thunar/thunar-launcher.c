xfce-mirror/thunar

commit 7df4b1aa0a9993d3bd447550d4fc86e7b12263f8

Avoid excessively checking whether files are executables (#180)

thunar/thunar-launcher.c
@@ -223,10 +223,10 @@ struct _ThunarLauncher
 
   gint                    n_files_to_process;
   gint                    n_directories_to_process;
-  gint                    n_executables_to_process;
   gint                    n_regulars_to_process;
   gboolean                files_to_process_trashable;
   gboolean                files_are_selected;
+  gboolean                files_are_all_executable;
   gboolean                single_directory_to_process;
 
   ThunarFile             *single_folder;
@@ -628,8 +628,8 @@ thunar_launcher_set_selected_files (ThunarComponent *component,
   launcher->files_to_process_trashable = TRUE;
   launcher->n_files_to_process         = 0;
   launcher->n_directories_to_process   = 0;
-  launcher->n_executables_to_process   = 0;
   launcher->n_regulars_to_process      = 0;
+  launcher->files_are_all_executable   = TRUE;
   launcher->single_directory_to_process = FALSE;
   launcher->single_folder = NULL;
   launcher->parent_folder = NULL;
@@ -654,8 +654,8 @@ thunar_launcher_set_selected_files (ThunarComponent *component,
         }
       else
         {
-          if (thunar_file_is_executable (lp->data))
-            ++launcher->n_executables_to_process;
+          if (launcher->files_are_all_executable && !thunar_file_is_executable (lp->data))
+            launcher->files_are_all_executable = FALSE;
           ++launcher->n_regulars_to_process;
         }
 
@@ -2986,7 +2986,7 @@ thunar_launcher_append_open_section (ThunarLauncher *launcher,
   applications = thunar_file_list_get_applications (launcher->files_to_process);
 
   /* Execute OR Open OR OpenWith */
-  if (G_UNLIKELY (launcher->n_executables_to_process == launcher->n_files_to_process))
+  if (G_UNLIKELY (launcher->files_are_all_executable))
     thunar_launcher_append_menu_item (launcher, GTK_MENU_SHELL (menu), THUNAR_LAUNCHER_ACTION_EXECUTE, FALSE);
   else if (G_LIKELY (launcher->n_directories_to_process >= 1))
     {

commit 953db576adb265ca3d75ba84a743710515c2ee5f

Dont show "execute" on context menu for folders
Regression introduced in 7df4b1aa

See as well !95

thunar/thunar-launcher.c
@@ -651,6 +651,7 @@ thunar_launcher_set_selected_files (ThunarComponent *component,
           || thunar_file_is_mountable (lp->data))
         {
           ++launcher->n_directories_to_process;
+          launcher->files_are_all_executable = FALSE;
         }
       else
         {