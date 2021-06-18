6690a237092433c1ba70c988d41b9a877a018320
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2021-06-18 15:19:20 +0300

    Free closure?

--- thunar/thunar-application.c
+++ thunar/thunar-application.c
@@ -2427,6 +2427,13 @@ thunar_application_empty_trash (ThunarApplication *application,
 
 
 
+typedef struct
+{
+    GHashTable *restore_open_table; /* string<directory_path>:GList<GFile*> */
+    GClosure   *restore_show_closure;
+} ShowAndSelectStruct;
+
+
 static void
 hash_table_entry_show_and_select_files (gpointer key, gpointer list, gpointer application)
 {
@@ -2453,12 +2460,14 @@ hash_table_entry_show_and_select_files (gpointer key, gpointer list, gpointer ap
 
 
 static void
-hash_table_show_and_select (GHashTable *restore_open_table)
+hash_table_show_and_select (ShowAndSelectStruct *show_select)
 {
   ThunarApplication *application = thunar_application_get();
-  g_hash_table_foreach (restore_open_table, hash_table_entry_show_and_select_files, application);
-  g_hash_table_destroy (restore_open_table);
+  g_hash_table_foreach (show_select->restore_open_table, hash_table_entry_show_and_select_files, application);
+  g_hash_table_destroy (show_select->restore_open_table);
   g_object_unref (application);
+  g_closure_unref (show_select->restore_show_closure);
+  g_free (show_select);
 }
 
 
@@ -2497,8 +2506,7 @@ thunar_application_restore_files (ThunarApplication *application,
   GList       *source_path_list   = NULL;
   GList       *target_path_list   = NULL;
   GList       *lp                 = NULL;
-  GHashTable  *restore_open_table = NULL; /* string<directory_path>:GList<GFile*> */
-  GClosure    *restore_show_closure = NULL;
+  ShowAndSelectStruct *show_select = NULL;
 
   _thunar_return_if_fail (parent == NULL || GDK_IS_SCREEN (parent) || GTK_IS_WIDGET (parent));
   _thunar_return_if_fail (THUNAR_IS_APPLICATION (application));
@@ -2506,10 +2514,12 @@ thunar_application_restore_files (ThunarApplication *application,
   if (open_original_folder)
     {
       _thunar_return_if_fail (new_files_closure == NULL);
-
-      restore_open_table = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, list_free_all_g_files);
-      restore_show_closure = g_cclosure_new_swap (G_CALLBACK (hash_table_show_and_select), restore_open_table, NULL);
-      new_files_closure = restore_show_closure;
+      show_select = g_malloc (sizeof (ShowAndSelectStruct));
+      show_select->restore_open_table = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, list_free_all_g_files);
+      show_select->restore_show_closure = g_cclosure_new_swap (G_CALLBACK (hash_table_show_and_select), show_select, NULL);
+      new_files_closure = show_select->restore_show_closure;
+      g_closure_ref (show_select->restore_show_closure);
+      g_closure_sink (show_select->restore_show_closure);
     }
 
   for (lp = trash_file_list; lp != NULL; lp = lp->next)
@@ -2536,14 +2546,14 @@ thunar_application_restore_files (ThunarApplication *application,
         {
           original_dir_path = g_strndup (original_uri, strlen (original_uri) - strlen (thunar_file_get_display_name (lp->data)));
 
-          if (g_hash_table_contains (restore_open_table, original_dir_path) == FALSE)
+          if (g_hash_table_contains (show_select->restore_open_table, original_dir_path) == FALSE)
             {
               GList *list = g_list_prepend (NULL, g_file_new_for_commandline_arg (original_uri));
-              g_hash_table_insert (restore_open_table, original_dir_path, list);
+              g_hash_table_insert (show_select->restore_open_table, original_dir_path, list);
             }
           else
             {
-              GList *list = g_hash_table_lookup (restore_open_table, original_dir_path);
+              GList *list = g_hash_table_lookup (show_select->restore_open_table, original_dir_path);
               list = g_list_append (list, g_file_new_for_commandline_arg (original_uri));
             }
         }
