xfce-mirror/thunar

commit c2a97b896f0497660dae273f72943a73caf2220a

Fix file-icon update when file is cut (Issue #588)
Worked in icn-view before, now works as well in list view.

thunar/thunar-window.c
@@ -1786,6 +1786,16 @@ thunar_window_action_switch_previous_tab (ThunarWindow *window)
 
 
 static void
+thunar_window_clipboard_manager_changed (GtkWidget *widget)
+{
+  ThunarWindow *window = THUNAR_WINDOW (widget);
+
+  thunar_window_reload (window, FALSE);
+}
+
+
+
+static void
 thunar_window_realize (GtkWidget *widget)
 {
   ThunarWindow *window = THUNAR_WINDOW (widget);
@@ -1798,7 +1808,7 @@ thunar_window_realize (GtkWidget *widget)
    */
   window->clipboard = thunar_clipboard_manager_get_for_display (gtk_widget_get_display (widget));
   g_signal_connect_swapped (G_OBJECT (window->clipboard), "changed",
-                            G_CALLBACK (gtk_widget_queue_draw), widget);
+                            G_CALLBACK (thunar_window_clipboard_manager_changed), widget);
 }
 
 

commit cfc47d65674e4341912e8a0a6fc0072cd2e8e595

Fix signal disconnect in thunar_window_unrealize
Commit c2a97b896f04 replaced the gtk_widget_queue_draw callback with
thunar_window_clipboard_manager_changed. The same change needs to be
made in thunar_window_unrealize, which was still referencing the old
function (gtk_widget_queue_draw).

Fixes: c2a97b896f04 ("Fix file-icon update when file is cut (Issue #588)")
Closes: https://gitlab.xfce.org/xfce/thunar/-/issues/653

thunar/thunar-window.c
@@ -1820,7 +1820,7 @@ thunar_window_unrealize (GtkWidget *widget)
   ThunarWindow *window = THUNAR_WINDOW (widget);
 
   /* disconnect from the clipboard manager */
-  g_signal_handlers_disconnect_by_func (G_OBJECT (window->clipboard), gtk_widget_queue_draw, widget);
+  g_signal_handlers_disconnect_by_func (G_OBJECT (window->clipboard), thunar_window_clipboard_manager_changed, widget);
 
   /* let the GtkWidget class unrealize the window */
   (*GTK_WIDGET_CLASS (thunar_window_parent_class)->unrealize) (widget);