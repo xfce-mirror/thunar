f078093fbbaef0ac16eebc7aaad2793b9cdf1d01
Theo Linkspfeifer <lastonestanding@tutanota.com>
2022-02-13 17:21:48 +0000

    Switch all labels to title case in preferences dialog

--- thunar/thunar-preferences-dialog.c
+++ thunar/thunar-preferences-dialog.c
@@ -518,7 +518,7 @@ thunar_preferences_dialog_init (ThunarPreferencesDialog *dialog)
   gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, TRUE, 0);
   gtk_widget_show (frame);
 
-  label = gtk_label_new (_("Window icon"));
+  label = gtk_label_new (_("Window Icon"));
   gtk_label_set_attributes (GTK_LABEL (label), thunar_pango_attr_list_bold ());
   gtk_frame_set_label_widget (GTK_FRAME (frame), label);
   gtk_widget_show (label);
@@ -981,7 +981,7 @@ thunar_preferences_dialog_init (ThunarPreferencesDialog *dialog)
   gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, TRUE, 0);
   gtk_widget_show (frame);
 
-  label = gtk_label_new (_("File transfer"));
+  label = gtk_label_new (_("File Transfer"));
   gtk_label_set_attributes (GTK_LABEL (label), thunar_pango_attr_list_bold ());
   gtk_frame_set_label_widget (GTK_FRAME (frame), label);
   gtk_widget_show (label);
