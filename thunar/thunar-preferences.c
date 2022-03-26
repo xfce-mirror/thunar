551c7bec8f901feb5cfc3bd027493c2254006e1f
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2022-03-26 23:57:56 +0200

    Toolbar: Add built-in buttons for Toolbar customization. - Search Toggle (Issue #621). - Reload button. - Zoom buttons.

--- thunar/thunar-preferences.c
+++ thunar/thunar-preferences.c
@@ -447,7 +447,7 @@ thunar_preferences_class_init (ThunarPreferencesClass *klass)
         g_param_spec_string ("last-toolbar-visible-buttons",
                              "LastToolbarVisibleButtons",
                              NULL,
-                             "1,1,1,1,1",
+                             "1,1,1,1,0,0,0,1,0,1",
                              EXO_PARAM_READWRITE);
 
   /**
@@ -459,7 +459,7 @@ thunar_preferences_class_init (ThunarPreferencesClass *klass)
         g_param_spec_string ("last-toolbar-item-order",
                              "LastToolbarButtonOrder",
                              NULL,
-                             "0,1,2,3,4",
+                             "0,1,2,3,4,5,6,7,8,9",
                              EXO_PARAM_READWRITE);
 
   /**
