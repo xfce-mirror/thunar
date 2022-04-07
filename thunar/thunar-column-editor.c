1b06a42fc01eb82dc48ad5dbf697c8bc4e75f8d0
Amrit Borah <elessar1802@gmail.com>
2022-04-07 13:29:26 +0530

    Fixed Move Up/Down & Show/Hide in column-editor

--- thunar/thunar-column-editor.c
+++ thunar/thunar-column-editor.c
@@ -137,16 +137,12 @@ thunar_column_editor_init (ThunarColumnEditor *column_editor)
   g_signal_connect_data (G_OBJECT (column_editor->column_model), "rows-reordered", G_CALLBACK (thunar_column_editor_update_buttons),
                          column_editor, NULL, G_CONNECT_AFTER | G_CONNECT_SWAPPED);
 
+  /* create a filter from the shared column model */
   filter = gtk_tree_model_filter_new (GTK_TREE_MODEL (column_editor->column_model),
                                       NULL);
   gtk_tree_model_filter_set_visible_func (GTK_TREE_MODEL_FILTER (filter),
                                           (GtkTreeModelFilterVisibleFunc) thunar_column_visible_func,
                                           NULL, NULL);
-  g_signal_connect_data (G_OBJECT (column_editor->column_model), "row-changed", G_CALLBACK (thunar_column_editor_update_buttons),
-                         column_editor, NULL, G_CONNECT_AFTER | G_CONNECT_SWAPPED);
-  g_signal_connect_data (G_OBJECT (column_editor->column_model), "rows-reordered", G_CALLBACK (thunar_column_editor_update_buttons),
-                         column_editor, NULL, G_CONNECT_AFTER | G_CONNECT_SWAPPED);
-
 
   /* setup the dialog */
   gtk_dialog_add_button (GTK_DIALOG (column_editor), _("_Close"), GTK_RESPONSE_CLOSE);
@@ -383,8 +379,11 @@ thunar_column_editor_move_down (GtkWidget          *button,
 {
   GtkTreeSelection *selection;
   GtkTreeModel     *model;
+  GtkTreeModel     *childModel;
   GtkTreeIter       iter1;
   GtkTreeIter       iter2;
+  GtkTreeIter       childIter1;
+  GtkTreeIter       childIter2;
 
   _thunar_return_if_fail (THUNAR_IS_COLUMN_EDITOR (column_editor));
   _thunar_return_if_fail (GTK_IS_BUTTON (button));
@@ -398,7 +397,15 @@ thunar_column_editor_move_down (GtkWidget          *button,
 
       /* determine the next iterator and exchange the rows */
       if (gtk_tree_model_iter_next (model, &iter2))
-        thunar_column_model_exchange (THUNAR_COLUMN_MODEL (model), &iter1, &iter2);
+      {
+        /* tree view's model is made from GTK_TREE_MODEL_FILTER,
+           hence fetching child model and child iter's */
+        gtk_tree_model_filter_convert_iter_to_child_iter (GTK_TREE_MODEL_FILTER (model), &childIter1, &iter1);
+        gtk_tree_model_filter_convert_iter_to_child_iter (GTK_TREE_MODEL_FILTER (model), &childIter2, &iter2);
+        childModel = gtk_tree_model_filter_get_model(GTK_TREE_MODEL_FILTER (model));
+
+        thunar_column_model_exchange (THUNAR_COLUMN_MODEL (childModel), &childIter1, &childIter2);
+      }
     }
 }
 
@@ -410,9 +417,12 @@ thunar_column_editor_move_up (GtkWidget          *button,
 {
   GtkTreeSelection *selection;
   GtkTreeModel     *model;
+  GtkTreeModel     *childModel;
   GtkTreePath      *path;
   GtkTreeIter       iter1;
   GtkTreeIter       iter2;
+  GtkTreeIter       childIter1;
+  GtkTreeIter       childIter2;
 
   _thunar_return_if_fail (THUNAR_IS_COLUMN_EDITOR (column_editor));
   _thunar_return_if_fail (GTK_IS_BUTTON (button));
@@ -428,8 +438,14 @@ thunar_column_editor_move_up (GtkWidget          *button,
           /* advance to the prev path */
           if (gtk_tree_path_prev (path) && gtk_tree_model_get_iter (model, &iter2, path))
             {
+              /* tree view's model is made from GTK_TREE_MODEL_FILTER,
+                 hence fetching child model and child iter's */
+              gtk_tree_model_filter_convert_iter_to_child_iter (GTK_TREE_MODEL_FILTER (model), &childIter1, &iter1);
+              gtk_tree_model_filter_convert_iter_to_child_iter (GTK_TREE_MODEL_FILTER (model), &childIter2, &iter2);
+              childModel = gtk_tree_model_filter_get_model(GTK_TREE_MODEL_FILTER (model));
+
               /* exchange the rows */
-              thunar_column_model_exchange (THUNAR_COLUMN_MODEL (model), &iter1, &iter2);
+              thunar_column_model_exchange (THUNAR_COLUMN_MODEL (childModel), &childIter1, &childIter2);
             }
 
           /* release the path */
@@ -478,7 +494,9 @@ thunar_column_editor_toggle_visibility (GtkWidget          *button,
 {
   GtkTreeSelection *selection;
   ThunarColumn      column;
+  GtkTreeModel     *model;
   GtkTreeIter       iter;
+  GtkTreeIter       childIter;
   gboolean          visible;
 
   _thunar_return_if_fail (THUNAR_IS_COLUMN_EDITOR (column_editor));
@@ -486,10 +504,14 @@ thunar_column_editor_toggle_visibility (GtkWidget          *button,
 
   /* determine the selected tree iterator */
   selection = gtk_tree_view_get_selection (GTK_TREE_VIEW (column_editor->tree_view));
-  if (gtk_tree_selection_get_selected (selection, NULL, &iter))
+  if (gtk_tree_selection_get_selected (selection, &model, &iter))
     {
+      /* tree view's model is made from GTK_TREE_MODEL_FILTER,
+         hence fetching child iter */
+      gtk_tree_model_filter_convert_iter_to_child_iter (GTK_TREE_MODEL_FILTER (model), &childIter, &iter);
+      
       /* determine the column for the iterator... */
-      column = thunar_column_model_get_column_for_iter (column_editor->column_model, &iter);
+      column = thunar_column_model_get_column_for_iter (column_editor->column_model, &childIter);
 
       /* ...determine the existing visbility setting... */
       visible = thunar_column_model_get_column_visible (column_editor->column_model, column);
@@ -518,6 +540,11 @@ thunar_column_editor_update_buttons (ThunarColumnEditor *column_editor)
     {
       /* determine the tree path for the iter */
       path = gtk_tree_model_get_path (model, &iter);
+
+      /* tree view's model is made from GTK_TREE_MODEL_FILTER,
+         hence fetching child path */
+      path = gtk_tree_model_filter_convert_path_to_child_path (GTK_TREE_MODEL_FILTER (model), path);
+      
       if (G_UNLIKELY (path == NULL))
         return;
 
