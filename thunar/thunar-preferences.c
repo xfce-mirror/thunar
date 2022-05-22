b91cf859f0903495253d6ba7584b01ee398c5538
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2022-05-22 12:59:22 +0300

    Toolbar: Add view-type entries

--- thunar/thunar-preferences.c
+++ thunar/thunar-preferences.c
@@ -449,7 +449,7 @@ thunar_preferences_class_init (ThunarPreferencesClass *klass)
         g_param_spec_string ("last-toolbar-visible-buttons",
                              "LastToolbarVisibleButtons",
                              NULL,
-                             "0,1,1,1,1,0,0,0,1,0,1",
+                             "0,1,1,1,1,0,0,0,0,0,0,1,0,1",
                              EXO_PARAM_READWRITE);
 
   /**
@@ -461,7 +461,7 @@ thunar_preferences_class_init (ThunarPreferencesClass *klass)
         g_param_spec_string ("last-toolbar-item-order",
                              "LastToolbarButtonOrder",
                              NULL,
-                             "0,1,2,3,4,5,6,7,8,9,10",
+                             "0,1,2,3,4,5,6,7,8,9,10,11,12,13",
                              EXO_PARAM_READWRITE);
 
   /**
