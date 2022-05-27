1b404cec8041fb1219255680902d73e980b7f065
Manuel Grießmayr <abspack@posteo.de>
2022-05-27 21:46:05 +0200

    replace deprecated function

--- thunar/thunar-launcher.c
+++ thunar/thunar-launcher.c
@@ -303,7 +303,7 @@ static XfceGtkActionEntry thunar_launcher_action_entries[] =
     { THUNAR_LAUNCHER_ACTION_MOUNT,            NULL,                                               "",                  XFCE_GTK_MENU_ITEM,       N_ ("_Mount"),                          N_ ("Mount the selected device"),                                                                NULL,                   G_CALLBACK (thunar_launcher_action_open),                },
     { THUNAR_LAUNCHER_ACTION_UNMOUNT,          NULL,                                               "",                  XFCE_GTK_MENU_ITEM,       N_ ("_Unmount"),                        N_ ("Unmount the selected device"),                                                              NULL,                   G_CALLBACK (thunar_launcher_action_unmount),             },
     { THUNAR_LAUNCHER_ACTION_EJECT,            NULL,                                               "",                  XFCE_GTK_MENU_ITEM,       N_ ("_Eject"),                          N_ ("Eject the selected device"),                                                                NULL,                   G_CALLBACK (thunar_launcher_action_eject),               },
-    { THUNAR_LAUNCHER_ACTION_EDIT_LAUNCHER,    NULL,                                               "",                  XFCE_GTK_IMAGE_MENU_ITEM, N_ ("_Edit Launcher"),                  N_ ("Edit the launcher"),                                                                        "gtk-edit",             G_CALLBACK (thunar_launcher_action_edit_launcher),       },
+    { THUNAR_LAUNCHER_ACTION_EDIT_LAUNCHER,    NULL,                                               "",                  XFCE_GTK_IMAGE_MENU_ITEM, N_ ("_Edit Launcher"),                  N_ ("Edit the selected launcher"),                                                               "gtk-edit",             G_CALLBACK (thunar_launcher_action_edit_launcher),       },
 };
 
 #define get_action_entry(id) xfce_gtk_get_action_entry_by_id(thunar_launcher_action_entries,G_N_ELEMENTS(thunar_launcher_action_entries),id)
@@ -1579,7 +1579,7 @@ thunar_launcher_append_menu_item (ThunarLauncher       *launcher,
         return xfce_gtk_menu_item_new_from_action_entry (action_entry, G_OBJECT (launcher), GTK_MENU_SHELL (menu));
 
       case THUNAR_LAUNCHER_ACTION_EDIT_LAUNCHER:
-        if (launcher->n_files_to_process == 1 && g_str_has_suffix (thunar_file_get_basename (launcher->files_to_process->data), ".desktop"))
+        if (launcher->n_files_to_process == 1 && thunar_file_is_desktop_file (launcher->files_to_process->data, NULL))
           return xfce_gtk_menu_item_new_from_action_entry (action_entry, G_OBJECT (launcher), GTK_MENU_SHELL (menu));
         else
           return NULL;
@@ -2984,27 +2984,29 @@ thunar_launcher_action_paste_into_folder (ThunarLauncher *launcher)
   return TRUE;
 }
 
+
+
 static void
 thunar_launcher_action_edit_launcher (ThunarLauncher *launcher)
 {
-  gchar *cmd = NULL, *uri = NULL, *display_name;
-  GError *error = NULL;
-
+  const gchar *display_name;
+  gchar       *cmd   = NULL,
+              *uri   = NULL;
+  GError      *error = NULL;
+  
   uri = thunar_file_dup_uri (launcher->files_to_process->data);
-  display_name = g_strdup (gdk_display_get_name (gdk_screen_get_display (gtk_widget_get_screen (launcher->widget))));
+  display_name = gdk_display_get_name (gdk_screen_get_display (gtk_widget_get_screen (launcher->widget)));
   cmd = g_strdup_printf ("exo-desktop-item-edit \"--display=%s\" \"%s\"", display_name, uri);
   
-  if(!xfce_spawn_command_line_on_screen (NULL, cmd, FALSE, FALSE, &error)) {
-    xfce_message_dialog ((GtkWindow *) launcher->widget, _("Launch Error"), "dialog-error",
-                         _("Unable to launch \"exo-desktop-item-edit\", which is required to edit launchers and links within Thunar."),
-                         error->message, XFCE_BUTTON_TYPE_MIXED, "window-close", _("_Close"), GTK_RESPONSE_ACCEPT, NULL);
-  }
+  if(!xfce_spawn_command_line (NULL, cmd, FALSE, FALSE, FALSE, &error))
+    thunar_dialogs_show_error (launcher->widget, error, _("Failed to edit launcher via command \"%s\""), cmd);
 
-  g_free (display_name);
-  g_free (uri);
   g_free (cmd);
+  g_free (uri);
   g_clear_error (&error);
 }
+
+
 
 /**
  * thunar_launcher_action_mount:
