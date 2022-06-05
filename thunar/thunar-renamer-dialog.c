6d055999dcf8fefc11f6cf5dafee264f6c4ae213
Alexander Schwinn <alexxcons@xfce.org>
2022-06-05 19:03:22 +0200

    remove duplicate include

--- thunar/thunar-renamer-dialog.c
+++ thunar/thunar-renamer-dialog.c
@@ -37,7 +37,6 @@
 #include <thunar/thunar-icon-factory.h>
 #include <thunar/thunar-icon-renderer.h>
 #include <thunar/thunar-action-manager.h>
-#include <thunar/thunar-action-manager.h>
 #include <thunar/thunar-menu.h>
 #include <thunar/thunar-private.h>
 #include <thunar/thunar-properties-dialog.h>
