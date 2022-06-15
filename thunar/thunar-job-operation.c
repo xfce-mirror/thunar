c89118eb896822097a4bc27d3d2cb60bb66b1071
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2022-06-15 10:15:10 +0530

    Add custom getters and setters for properties

--- thunar/thunar-job-operation.c
+++ thunar/thunar-job-operation.c
@@ -24,14 +24,23 @@ enum
   N_PROPERTIES,
 };
 
+static void     thunar_job_operation_finalize           (GObject          *object);
+static void     thunar_job_operation_get_property       (GObject          *object,
+                                                         guint             prop_id,
+                                                         GValue           *value,
+                                                         GParamSpec       *pspec);
+static void     thunar_job_operation_set_property       (GObject          *object,
+                                                         guint             prop_id,
+                                                         const GValue     *value,
+                                                         GParamSpec       *pspec);
+
 struct _ThunarJobOperation
 {
   GObject __parent__;
 
-  /* DOUBT:
-   * Should these also be object properties instead of internal members? */
-  GList         *source_file_list;
-  GList         *target_file_list;
+  gchar *operation_type;
+  GList *source_file_list;
+  GList *target_file_list;
 };
 
 G_DEFINE_TYPE (ThunarJobOperation, thunar_job_operation, G_TYPE_OBJECT)
@@ -44,8 +53,9 @@ thunar_job_operation_class_init (ThunarJobOperationClass *klass)
   GObjectClass *gobject_class;
 
   gobject_class = G_OBJECT_CLASS (klass);
-  /* gobject_class->get_property = thunar_job_operation_get_property; */
-  /* gobject_class->set_property = thunar_job_operation_get_property; */
+  gobject_class->finalize = thunar_job_operation_finalize;
+  gobject_class->get_property = thunar_job_operation_get_property;
+  gobject_class->set_property = thunar_job_operation_set_property;
 
   /**
    * ThunarJobOperation:operation-type:
@@ -59,11 +69,12 @@ thunar_job_operation_class_init (ThunarJobOperationClass *klass)
                          NULL,
                          G_PARAM_CONSTRUCT_ONLY | G_PARAM_READABLE);
   /* DOUBT:
-   * Ahould this be an enum instead of a string? Since we want it to be limited
+   * Should this be an enum instead of a string? Since we want it to be limited
    * to a specific set of values.
    * Raises question about where to put the enum definition, and what it should be named.
    * */
 
+  g_object_class_install_properties (gobject_class, N_PROPERTIES, job_operation_props);
 }
 
 static void
@@ -72,3 +83,50 @@ thunar_job_operation_init (ThunarJobOperation *self)
   self->source_file_list = NULL;
   self->target_file_list = NULL;
 }
+
+static void
+thunar_job_operation_finalize (GObject *object)
+{
+  (*G_OBJECT_CLASS (thunar_job_operation_parent_class)->finalize) (object);
+}
+
+static void
+thunar_job_operation_get_property (GObject    *object,
+                                   guint       prop_id,
+                                   GValue     *value,
+                                   GParamSpec *pspec)
+{
+  ThunarJobOperation *self = THUNAR_JOB_OPERATION (object);
+
+  switch (prop_id)
+    {
+    case PROP_OPERATION_TYPE:
+      g_value_set_string (value, self->operation_type);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+static void
+thunar_job_operation_set_property  (GObject      *object,
+                                    guint         prop_id,
+                                    const GValue *value,
+                                    GParamSpec   *pspec)
+  {
+    ThunarJobOperation *self = THUNAR_JOB_OPERATION (object);
+  
+    switch (prop_id)
+      {
+      case PROP_OPERATION_TYPE:
+        g_free (self->operation_type);
+        self->operation_type = g_value_dup_string (value);
+        break;
+  
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+        break;
+      }
+  }
