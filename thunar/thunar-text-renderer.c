85e4aba1d8dc2c3dfcb14c8ec868e090da15e6b0
Amrit Borah <elessar1802@gmail.com>
2022-07-25 17:28:08 +0530

    hid the original selected highlight
    .

--- thunar/thunar-text-renderer.c
+++ thunar/thunar-text-renderer.c
@@ -236,6 +236,22 @@ thunar_text_renderer_new (void)
 
 
 static void
+thunar_text_renderer_clear_background (cairo_t   *cr,
+                                                GtkWidget *widget)
+{
+  GtkStyleContext *context;
+  GdkRGBA         *color;
+
+  context = gtk_widget_get_style_context (widget);
+  gtk_style_context_get (context, GTK_STATE_FLAG_NORMAL, GTK_STYLE_PROPERTY_BACKGROUND_COLOR, &color, NULL);
+  gdk_cairo_set_source_rgba (cr, color);
+
+  cairo_paint (cr);
+}
+
+
+
+static void
 thunar_text_renderer_render (GtkCellRenderer      *cell,
                              cairo_t              *cr,
                              GtkWidget            *widget,
@@ -244,7 +260,13 @@ thunar_text_renderer_render (GtkCellRenderer      *cell,
                              GtkCellRendererState  flags)
 {
   if (THUNAR_TEXT_RENDERER (cell)->highlighting_enabled)
-    thunar_util_clip_view_background (cell, cr, background_area, widget, flags);
+    {
+      /* This should paint on top of the current surface. This should hide the highlight
+         that is drawn by the default render function of GtkCellRendererText */
+      thunar_text_renderer_clear_background (cr, widget);
+
+      thunar_util_clip_view_background (cell, cr, background_area, widget, flags);
+    }
 
   /* we only needed to manipulate the background_area, otherwise everything remains the same.
      Hence, we are simply running the original render function now */
