cd2e181cf40022f06ad56dca0891e660ce078c34
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2022-09-20 21:21:39 +0530

    Add enum for ThunarItemsAsFolderSize

--- thunar/thunar-enum-types.c
+++ thunar/thunar-enum-types.c
@@ -669,3 +669,23 @@ thunar_job_operation_kind_get_type (void)
 
   return type;
 }
+
+GType thunar_items_as_folder_size_get_type (void)
+{
+  static GType type = G_TYPE_INVALID;
+
+  if (G_UNLIKELY (type == G_TYPE_INVALID))
+    {
+      static const GEnumValue values[] =
+      {
+        { THUNAR_ITEMS_AS_FOLDER_SIZE_NEVER,       "THUNAR_ITEMS_AS_FOLDER_SIZE_NEVER",       N_("Never") },
+        { THUNAR_ITEMS_AS_FOLDER_SIZE_ONLY_LOCAL,  "THUNAR_ITEMS_AS_FOLDER_SIZE_ONLY_LOCAL",  N_("Only for local files") },
+        { THUNAR_ITEMS_AS_FOLDER_SIZE_ALWAYS,      "THUNAR_ITEMS_AS_FOLDER_SIZE_ALWAYS",      N_("Always") },
+        { 0,                                       NULL,                                      NULL }
+      };
+
+      type = g_enum_register_static ("ThunarItemsAsFolderSize", values);
+    }
+
+  return type;
+}
