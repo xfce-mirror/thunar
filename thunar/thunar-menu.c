ba1da61b65427a86eacb2bed80d1e96e078b79ff
Alexander Schwinn <alexxcons@xfce.org>
2022-10-06 09:24:25 +0200

    Move undo/redo menu item from action manager to window (Issue #819)
    - Rerquired in order to use it in the toolbar

--- thunar/thunar-menu.c
+++ thunar/thunar-menu.c
@@ -252,15 +252,6 @@ thunar_menu_add_sections (ThunarMenu         *menu,
 
   _thunar_return_val_if_fail (THUNAR_IS_MENU (menu), FALSE);
 
-  if (menu_sections & THUNAR_MENU_SECTION_UNDO)
-    {
-      item_added = (thunar_action_manager_append_menu_item (menu->action_mgr, GTK_MENU_SHELL (menu), THUNAR_ACTION_MANAGER_ACTION_UNDO, force) != NULL);
-      item_added = (thunar_action_manager_append_menu_item (menu->action_mgr, GTK_MENU_SHELL (menu), THUNAR_ACTION_MANAGER_ACTION_REDO, force) != NULL);
-
-      if (item_added)
-        xfce_gtk_menu_append_separator (GTK_MENU_SHELL (menu));
-    }
-
   if (menu_sections & THUNAR_MENU_SECTION_CREATE_NEW_FILES)
     {
       item_added = FALSE;
