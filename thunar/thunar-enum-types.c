31c8da11fdc0de27809e108aa82f445b4c9fd2a8
Alexander Schwinn <alexxcons@xfce.org>
2022-10-25 19:46:40 +0000

    remove duplicated method to prevent warning

--- thunar/thunar-enum-types.c
+++ thunar/thunar-enum-types.c
@@ -770,23 +770,3 @@ GType thunar_folder_item_count_get_type (void)
 
   return type;
 }
-
-GType
-thunar_items_as_folder_size_get_type (void)
-{
-  static GType type = G_TYPE_INVALID;
-
-  if (G_UNLIKELY (type == G_TYPE_INVALID))
-    {
-      static const GEnumValue values[] =
-      {
-        { THUNAR_FOLDER_ITEM_COUNT_NEVER,       "THUNAR_FOLDER_ITEM_COUNT_NEVER",       N_("Never") },
-        { THUNAR_FOLDER_ITEM_COUNT_ONLY_LOCAL,  "THUNAR_FOLDER_ITEM_COUNT_ONLY_LOCAL",  N_("Only for local files") },
-        { THUNAR_FOLDER_ITEM_COUNT_ALWAYS,      "THUNAR_FOLDER_ITEM_COUNT_ALWAYS",      N_("Always") },
-        { 0,                                     NULL,                                  NULL }
-      };
-
-      type = g_enum_register_static ("ThunarItemsAsFolderSize", values);
-    }
-  return type;
-}
