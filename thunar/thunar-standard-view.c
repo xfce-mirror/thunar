ad6310b8dc3258e3582ee00ef5fb6b75d4d2fa12
Brian J. Tarricone <brian@tarricone.org>
2022-11-01 02:09:57 -0700

    Fix blurriness in ThunarImage when UI scaling > 1
    MR !311

--- thunar/thunar-standard-view.c
+++ thunar/thunar-standard-view.c
@@ -120,7 +120,7 @@ static void                 thunar_standard_view_set_property               (GOb
                                                                              guint                     prop_id,
                                                                              const GValue             *value,
                                                                              GParamSpec               *pspec);
-static void                 thunar_standard_view_scale_factor_changed       (GObject                  *object,
+static void                 thunar_standard_view_scale_changed              (GObject                  *object,
                                                                              GParamSpec               *pspec,
                                                                              gpointer                  user_data);
 static void                 thunar_standard_view_realize                    (GtkWidget                *widget);
@@ -843,7 +843,7 @@ thunar_standard_view_init (ThunarStandardView *standard_view)
   g_object_bind_property (G_OBJECT (standard_view), "zoom-level", G_OBJECT (standard_view->icon_renderer), "size", G_BINDING_SYNC_CREATE);
   g_object_bind_property (G_OBJECT (standard_view->icon_renderer), "size", G_OBJECT (standard_view->priv->thumbnailer), "thumbnail-size", G_BINDING_SYNC_CREATE);
   g_object_bind_property (G_OBJECT (standard_view->preferences), "misc-highlighting-enabled", G_OBJECT (standard_view->icon_renderer), "highlighting-enabled", G_BINDING_SYNC_CREATE);
-  g_signal_connect (G_OBJECT (standard_view), "notify::scale-factor", G_CALLBACK (thunar_standard_view_scale_factor_changed), NULL);
+  g_signal_connect (G_OBJECT (standard_view), "notify::scale-factor", G_CALLBACK (thunar_standard_view_scale_changed), NULL);
 
   /* setup the name renderer */
   standard_view->name_renderer = thunar_text_renderer_new ();
@@ -1251,9 +1251,9 @@ thunar_standard_view_set_property (GObject      *object,
 
 
 static void
-thunar_standard_view_scale_factor_changed (GObject    *object,
-                                           GParamSpec *pspec,
-                                           gpointer    user_data)
+thunar_standard_view_scale_changed (GObject    *object,
+                                    GParamSpec *pspec,
+                                    gpointer    user_data)
 {
     gtk_widget_queue_draw (GTK_WIDGET (object));
 }
