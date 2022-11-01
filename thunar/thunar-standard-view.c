02f47a46665ec63b03930a76fc1f3250d060500b
Brian J. Tarricone <brian@tarricone.org>
2022-11-01 14:10:11 -0700

    Fix blurriness in drag icons when UI scale > 1

--- thunar/thunar-standard-view.c
+++ thunar/thunar-standard-view.c
@@ -3383,9 +3383,11 @@ thunar_standard_view_drag_begin (GtkWidget          *view,
                                  GdkDragContext     *context,
                                  ThunarStandardView *standard_view)
 {
-  ThunarFile *file;
-  GdkPixbuf  *icon;
-  gint        size;
+  ThunarFile      *file;
+  GdkPixbuf       *icon;
+  cairo_surface_t *surface;
+  gint             size;
+  gint             scale_factor;
 
   /* release the drag path list (just in case the drag-end wasn't fired before) */
   thunar_g_list_free_full (standard_view->priv->drag_g_file_list);
@@ -3400,9 +3402,15 @@ thunar_standard_view_drag_begin (GtkWidget          *view,
         {
           /* generate an icon based on that file */
           g_object_get (G_OBJECT (standard_view->icon_renderer), "size", &size, NULL);
-          icon = thunar_icon_factory_load_file_icon (standard_view->icon_factory, file, THUNAR_FILE_ICON_STATE_DEFAULT, size);
-          gtk_drag_set_icon_pixbuf (context, icon, 0, 0);
-          g_object_unref (G_OBJECT (icon));
+          scale_factor = gtk_widget_get_scale_factor (view);
+          icon = thunar_icon_factory_load_file_icon (standard_view->icon_factory, file, THUNAR_FILE_ICON_STATE_DEFAULT, size * scale_factor);
+          if (G_LIKELY (icon != NULL))
+            {
+              surface = gdk_cairo_surface_create_from_pixbuf (icon, scale_factor, gtk_widget_get_window (view));
+              g_object_unref (G_OBJECT (icon));
+              gtk_drag_set_icon_surface (context, surface);
+              cairo_surface_destroy (surface);
+            }
 
           /* release the file */
           g_object_unref (G_OBJECT (file));
