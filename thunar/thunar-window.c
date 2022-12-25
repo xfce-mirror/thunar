b90bf5c6b1802a80ca8792912f6cf846eb0f5a37
Alexander Schwinn <alexxcons@xfce.org>
2022-12-25 01:20:30 +0100

    Dont show toolbar menu for back/forward (Issue #983)

--- thunar/thunar-window.c
+++ thunar/thunar-window.c
@@ -5991,7 +5991,9 @@ thunar_window_create_toolbar_item_from_action (ThunarWindow       *window,
   toolbar_item = xfce_gtk_tool_button_new_from_action_entry (get_action_entry (action), G_OBJECT (window), GTK_TOOLBAR (window->location_toolbar));
   g_object_set_data_full (G_OBJECT (toolbar_item), "label", g_strdup (get_action_entry (action)->menu_item_label_text), g_free);
   g_object_set_data_full (G_OBJECT (toolbar_item), "icon", g_strdup (get_action_entry (action)->menu_item_icon_name), g_free);
-  g_signal_connect_after (G_OBJECT (toolbar_item), "button-press-event", G_CALLBACK (thunar_window_toolbar_button_clicked), G_OBJECT (window));
+  
+  if (action != THUNAR_WINDOW_ACTION_BACK && action != THUNAR_WINDOW_ACTION_FORWARD)
+    g_signal_connect_after (G_OBJECT (toolbar_item), "button-press-event", G_CALLBACK (thunar_window_toolbar_button_clicked), G_OBJECT (window));
 
   thunar_g_object_set_guint_data (G_OBJECT (toolbar_item), "default-order", item_order);
 
