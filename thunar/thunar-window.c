xfce-mirror/thunar

commit d16b2f34d93abdeecffe00a3818e5dc50cdf693f

Prevent segfault for location selector (Issue #994)
When switching to location style after 'open location'

thunar/thunar-window.c
@@ -3215,7 +3215,7 @@ thunar_window_start_open_location (ThunarWindow *window,
 
       /* temporary show the location toolbar, even if it is normally hidden */
       gtk_widget_show (window->location_toolbar);
-      thunar_location_bar_request_entry (THUNAR_LOCATION_BAR (window->location_bar), initial_text);
+      thunar_location_bar_request_entry (THUNAR_LOCATION_BAR (window->location_bar), initial_text, FALSE);
 
       thunar_window_update_search (window);
       window->is_searching = TRUE;
@@ -3227,11 +3227,19 @@ thunar_window_start_open_location (ThunarWindow *window,
     }
   else /* location edit */
     {
+      gchar *last_location_bar = NULL;
+      gboolean temporary_till_focus_lost = FALSE;
+
+      g_object_get (window->preferences, "last-location-bar", &last_location_bar, NULL);
+      if (g_strcmp0 (last_location_bar, g_type_name (THUNAR_TYPE_LOCATION_BUTTONS)) == 0)
+        temporary_till_focus_lost = TRUE;
+      g_free (last_location_bar);
+
       thunar_window_action_cancel_search (window);
 
       /* temporary show the location toolbar, even if it is normally hidden */
       gtk_widget_show (window->location_toolbar);
-      thunar_location_bar_request_entry (THUNAR_LOCATION_BAR (window->location_bar), initial_text);
+      thunar_location_bar_request_entry (THUNAR_LOCATION_BAR (window->location_bar), initial_text, temporary_till_focus_lost);
     }
 }
 
@@ -3251,7 +3259,7 @@ thunar_window_resume_search (ThunarWindow *window,
 
   /* temporary show the location toolbar, even if it is normally hidden */
   gtk_widget_show (window->location_toolbar);
-  thunar_location_bar_request_entry (THUNAR_LOCATION_BAR (window->location_bar), initial_text);
+  thunar_location_bar_request_entry (THUNAR_LOCATION_BAR (window->location_bar), initial_text, FALSE);
 
   /* change to search UI and options */
   g_free (window->search_query);
@@ -3684,6 +3692,8 @@ thunar_window_action_locationbar_entry (ThunarWindow *window)
   entry_checked = (g_strcmp0 (last_location_bar, g_type_name (THUNAR_TYPE_LOCATION_ENTRY)) == 0);
   g_free (last_location_bar);
 
+  thunar_window_action_cancel_search (window);
+
   if (entry_checked)
     g_object_set (window->preferences, "last-location-bar", g_type_name (G_TYPE_NONE), NULL);
   else
@@ -3706,6 +3716,8 @@ thunar_window_action_locationbar_buttons (ThunarWindow *window)
   g_object_get (window->preferences, "last-location-bar", &last_location_bar, NULL);
   buttons_checked = (g_strcmp0 (last_location_bar, g_type_name (THUNAR_TYPE_LOCATION_BUTTONS)) == 0);
   g_free (last_location_bar);
+
+  thunar_window_action_cancel_search (window);
 
   if (buttons_checked)
     g_object_set (window->preferences, "last-location-bar", g_type_name (G_TYPE_NONE), NULL);

thunar/thunar-location-bar.c
@@ -82,7 +82,8 @@ static GtkWidget   *thunar_location_bar_install_widget             (ThunarLocati
 static void         thunar_location_bar_settings_changed           (ThunarLocationBar    *bar);
 static void         thunar_location_bar_on_enry_edit_done          (ThunarLocationEntry  *entry,
                                                                     ThunarLocationBar    *bar);
-
+static void         thunar_location_bar_request_temp_entry         (ThunarLocationBar    *bar,
+                                                                    const gchar          *initial_text);
 
 
 G_DEFINE_TYPE_WITH_CODE (ThunarLocationBar, thunar_location_bar, GTK_TYPE_BIN,
@@ -275,7 +276,7 @@ thunar_location_bar_install_widget (ThunarLocationBar    *bar,
         {
           bar->locationButtons = gtk_widget_new (THUNAR_TYPE_LOCATION_BUTTONS, "current-directory", NULL, NULL);
           g_object_ref (bar->locationButtons);
-          g_signal_connect_swapped (bar->locationButtons, "entry-requested", G_CALLBACK (thunar_location_bar_request_entry), bar);
+          g_signal_connect_swapped (bar->locationButtons, "entry-requested", G_CALLBACK (thunar_location_bar_request_temp_entry), bar);
           g_signal_connect_swapped (bar->locationButtons, "change-directory", G_CALLBACK (thunar_navigator_change_directory), THUNAR_NAVIGATOR (bar));
           g_signal_connect_swapped (bar->locationButtons, "open-new-tab", G_CALLBACK (thunar_navigator_open_new_tab), THUNAR_NAVIGATOR (bar));
         }
@@ -307,11 +308,21 @@ thunar_location_bar_on_enry_edit_done (ThunarLocationEntry *entry,
 
 
 
+static void
+thunar_location_bar_request_temp_entry (ThunarLocationBar *bar,
+                                        const gchar       *initial_text)
+{
+  thunar_location_bar_request_entry (bar, initial_text, TRUE);
+}
+
+
+
 /**
  * thunar_location_bar_request_entry
  * @bar          : The #ThunarLocationBar
  * @initial_text : The initial text to be placed inside the entry, or NULL to
  *                 use the path of the current directory.
+ * @temporary_till_focus_lost : Revert to the previous location bar style when focus got lost
  *
  * Makes the location bar display an entry with the given text and places the cursor
  * accordingly. If the currently displayed location widget is a path bar, it will be
@@ -320,7 +331,8 @@ thunar_location_bar_on_enry_edit_done (ThunarLocationEntry *entry,
  */
 void
 thunar_location_bar_request_entry (ThunarLocationBar *bar,
-                                   const gchar       *initial_text)
+                                   const gchar       *initial_text,
+                                   gboolean           temporary_till_focus_lost)
 {
   GtkWidget *child;
 
@@ -340,7 +352,11 @@ thunar_location_bar_request_entry (ThunarLocationBar *bar,
       thunar_location_entry_accept_focus (THUNAR_LOCATION_ENTRY (child), initial_text);
     }
 
-  g_signal_connect (child, "edit-done", G_CALLBACK (thunar_location_bar_on_enry_edit_done), bar);
+  if (temporary_till_focus_lost)
+    {
+      thunar_location_entry_enable_edit_done_once (THUNAR_LOCATION_ENTRY (child));
+      g_signal_connect (child, "edit-done", G_CALLBACK (thunar_location_bar_on_enry_edit_done), bar);
+    }
 }
 
 
@@ -379,6 +395,9 @@ thunar_location_bar_cancel_search (ThunarLocationBar *bar)
 {
   if (bar->locationEntry != NULL)
     thunar_location_entry_cancel_search (THUNAR_LOCATION_ENTRY (bar->locationEntry));
+  
+  /* Recover the previous location bar style (BUTTONS/ENTRY) after search is done */
+  thunar_location_bar_settings_changed (bar);
 }
 
 

thunar/thunar-location-bar.h
@@ -33,14 +33,13 @@ typedef struct _ThunarLocationBar      ThunarLocationBar;
 #define THUNAR_LOCATION_BAR_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), THUNAR_TYPE_LOCATION_BAR, ThunarLocationBarClass))
 
 
-GType      thunar_location_bar_get_type      (void) G_GNUC_CONST;
+GType      thunar_location_bar_get_type         (void) G_GNUC_CONST;
 
-GtkWidget *thunar_location_bar_new           (void);
-
-void       thunar_location_bar_request_entry (ThunarLocationBar *bar, const gchar *initial_text);
-
-void       thunar_location_bar_cancel_search (ThunarLocationBar *bar);
-
+GtkWidget *thunar_location_bar_new              (void);
+void       thunar_location_bar_request_entry    (ThunarLocationBar *bar,
+                                                 const gchar       *initial_text,
+                                                 gboolean           temporary_till_focus_lost);
+void       thunar_location_bar_cancel_search    (ThunarLocationBar *bar);
 gchar*     thunar_location_bar_get_search_query (ThunarLocationBar *entry);
 
 G_END_DECLS;

thunar/thunar-location-entry.c
@@ -94,7 +94,6 @@ struct _ThunarLocationEntry
   GtkWidget    *path_entry;
 
   gboolean      right_click_occurred;
-  gboolean      is_searching;
 };
 
 
@@ -285,18 +284,6 @@ thunar_location_entry_accept_focus (ThunarLocationEntry *location_entry,
   /* give the keyboard focus to the path entry */
   gtk_widget_grab_focus (location_entry->path_entry);
 
-  /* setup search if the initial_text signifies a search operation, otherwise setup location editing */
-  location_entry->is_searching = (initial_text != NULL && thunar_util_is_a_search_query (initial_text) == TRUE);
-  if (location_entry->is_searching)
-    {
-      g_signal_handlers_disconnect_by_func (location_entry->path_entry, G_CALLBACK (thunar_location_entry_emit_edit_done), location_entry);
-    }
-  else
-    {
-      /* make sure the edit-done signal is emitted upon moving the focus somewhere else */
-      g_signal_connect_swapped (location_entry->path_entry, "focus-out-event", G_CALLBACK (thunar_location_entry_emit_edit_done), location_entry);
-    }
-
   /* check if we have an initial text for the location bar */
   if (G_LIKELY (initial_text != NULL))
     {
@@ -446,12 +433,27 @@ thunar_location_entry_reset (ThunarLocationEntry *location_entry)
 
 
 
+/**
+ * thunar_location_entry_enable_edit_done_once:
+ * @entry: The #ThunarLocationEntry
+ *
+ * Request to emit the 'edit done' once, whenever the focus on the widget got lost
+ */
+void
+thunar_location_entry_enable_edit_done_once (ThunarLocationEntry *location_entry)
+{
+  g_signal_connect_swapped (location_entry->path_entry, "focus-out-event", G_CALLBACK (thunar_location_entry_emit_edit_done), location_entry);
+}
+
+
+
 static void
 thunar_location_entry_emit_edit_done (ThunarLocationEntry *entry)
 {
-  /* do not emit the signal if the context menu was opened or a search is active */
-  if (entry->right_click_occurred == FALSE && entry->is_searching == FALSE)
+  /* do not emit the signal if the context menu was opened */
+  if (entry->right_click_occurred == FALSE)
     {
+      g_signal_handlers_disconnect_by_func (entry->path_entry, G_CALLBACK (thunar_location_entry_emit_edit_done), entry);
       g_signal_emit_by_name (entry, "edit-done");
     }
 
@@ -469,7 +471,6 @@ thunar_location_entry_emit_edit_done (ThunarLocationEntry *entry)
 void
 thunar_location_entry_cancel_search (ThunarLocationEntry *entry)
 {
-  entry->is_searching = FALSE;
   entry->right_click_occurred = FALSE;
   thunar_location_entry_emit_edit_done (entry);
 

thunar/thunar-location-entry.h
@@ -34,12 +34,13 @@ typedef struct _ThunarLocationEntry      ThunarLocationEntry;
 #define THUNAR_IS_LOCATION_ENTRY_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), THUNAR_TYPE_LOCATION_ENTRY))
 #define THUNAR_LOCATION_ENTRY_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), THUNAR_TYPE_LOCATION_ENTRY, ThunarLocationEntryClass))
 
-GType      thunar_location_entry_get_type         (void) G_GNUC_CONST;
+GType      thunar_location_entry_get_type              (void) G_GNUC_CONST;
 
-void       thunar_location_entry_accept_focus     (ThunarLocationEntry *entry, const gchar *initial_text);
-
-void       thunar_location_entry_cancel_search    (ThunarLocationEntry *entry);
-gchar     *thunar_location_entry_get_search_query (ThunarLocationEntry *entry);
+void       thunar_location_entry_accept_focus          (ThunarLocationEntry *entry,
+                                                        const gchar         *initial_text);
+void       thunar_location_entry_cancel_search         (ThunarLocationEntry *entry);
+gchar     *thunar_location_entry_get_search_query      (ThunarLocationEntry *entry);
+void       thunar_location_entry_enable_edit_done_once (ThunarLocationEntry *location_entry);
 
 G_END_DECLS;
 

commit 19be7ac46975371cdc08710f7ea14bdaa1633313

Keep hidden toolbar hidden after Ctrl+L (#1011)
And after search.

Regression for Ctrl+L was introduced by d16b2f34

thunar/thunar-window.c
@@ -3234,8 +3234,9 @@ thunar_window_start_open_location (ThunarWindow *window,
       gchar *last_location_bar = NULL;
       gboolean temporary_till_focus_lost = FALSE;
 
+      /* If the style is already 'location entry', we dont need to care about 'focus-lost' */
       g_object_get (window->preferences, "last-location-bar", &last_location_bar, NULL);
-      if (g_strcmp0 (last_location_bar, g_type_name (THUNAR_TYPE_LOCATION_BUTTONS)) == 0)
+      if (g_strcmp0 (last_location_bar, g_type_name (THUNAR_TYPE_LOCATION_ENTRY)) != 0)
         temporary_till_focus_lost = TRUE;
       g_free (last_location_bar);
 
@@ -3369,6 +3370,10 @@ thunar_window_action_cancel_search (ThunarWindow *window)
   g_signal_handlers_block_by_func (G_OBJECT (window->location_toolbar_item_search), thunar_window_action_search, window);
   gtk_toggle_tool_button_set_active (GTK_TOGGLE_TOOL_BUTTON (window->location_toolbar_item_search), FALSE);
   g_signal_handlers_unblock_by_func (G_OBJECT (window->location_toolbar_item_search), thunar_window_action_search, window);
+  
+  /* bring back the original location bar style (relevant if the bar is hidden) */
+  thunar_window_update_location_bar_visible (window);
+
   /* required in case of shortcut activation, in order to signal that the accel key got handled */
   return TRUE;
 }