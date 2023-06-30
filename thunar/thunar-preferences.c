3d6320618c8a3e35cb70a567523d2875608d5f23
recallmenot <aac@a.com>
2023-06-30 08:56:13 +0200

    copy only-idle build fix

--- thunar/thunar-preferences.c
+++ thunar/thunar-preferences.c
@@ -980,7 +980,6 @@ thunar_preferences_class_init (ThunarPreferencesClass *klass)
                          NULL,
                          THUNAR_TYPE_PARALLEL_COPY_MODE,
                          THUNAR_PARALLEL_COPY_MODE_ONLY_LOCAL,
-                         THUNAR_PARALLEL_COPY_MODE_ONLY_LOCAL_IDLE_DEVICE,
                          EXO_PARAM_READWRITE);
 
   /**
