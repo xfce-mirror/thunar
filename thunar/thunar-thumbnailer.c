xfce-mirror/thunar

commit 6515c58767ef3f46308705559f7ea001940a9c3b

Move thumbnail request handling to the icon-renderer
- vastly simplified thumbnail request logic
- significant code reduction
- Fixes #1145

MR !396

thunar/thunar-thumbnailer.c
@@ -441,12 +441,12 @@ thunar_thumbnailer_begin_job (ThunarThumbnailer *thumbnailer,
       /* the icon factory only loads icons for regular files and folders */
       if (!thunar_file_is_regular (lp->data) && !thunar_file_is_directory (lp->data))
         {
-          thunar_file_set_thumb_state (lp->data, THUNAR_FILE_THUMB_STATE_NONE);
+          thunar_file_set_thumb_state (lp->data, THUNAR_FILE_THUMB_STATE_NONE, thumbnail_size);
           continue;
         }
 
       /* get the current thumb state */
-      thumb_state = thunar_file_get_thumb_state (lp->data);
+      thumb_state = thunar_file_get_thumb_state (lp->data, thumbnail_size);
 
       if (job->lazy_checks)
         {
@@ -467,7 +467,7 @@ thunar_thumbnailer_begin_job (ThunarThumbnailer *thumbnailer,
           if (max_size > 0 && thunar_file_get_size (lp->data) > max_size)
             continue;
 
-          supported_files = g_list_prepend (supported_files, lp->data);
+          supported_files = g_list_prepend (supported_files, g_object_ref (lp->data));
           n_items++;
         }
       else
@@ -478,9 +478,9 @@ thunar_thumbnailer_begin_job (ThunarThumbnailer *thumbnailer,
 
           /* test if a thumbnail can be found */
           if (thumbnail_path != NULL && g_file_test (thumbnail_path, G_FILE_TEST_EXISTS))
-            thunar_file_set_thumb_state (lp->data, THUNAR_FILE_THUMB_STATE_READY);
+            thunar_file_set_thumb_state (lp->data, THUNAR_FILE_THUMB_STATE_READY, thumbnail_size);
           else
-            thunar_file_set_thumb_state (lp->data, THUNAR_FILE_THUMB_STATE_NONE);
+            thunar_file_set_thumb_state (lp->data, THUNAR_FILE_THUMB_STATE_NONE, thumbnail_size);
         }
     }
 
@@ -495,7 +495,7 @@ thunar_thumbnailer_begin_job (ThunarThumbnailer *thumbnailer,
       for (lp = supported_files, n = 0; lp != NULL; lp = lp->next, ++n)
         {
           /* set the thumbnail state to loading */
-          thunar_file_set_thumb_state (lp->data, THUNAR_FILE_THUMB_STATE_LOADING);
+          thunar_file_set_thumb_state (lp->data, THUNAR_FILE_THUMB_STATE_LOADING, thumbnail_size);
 
           /* save URI and MIME hint in the arrays */
           uris[n] = thunar_file_dup_uri (lp->data);
@@ -534,12 +534,12 @@ thunar_thumbnailer_begin_job (ThunarThumbnailer *thumbnailer,
       g_free (mime_hints);
       g_strfreev (uris);
 
-      /* free the list of supported files */
-      g_list_free (supported_files);
-
       /* free the list of files passed in */
       g_list_free_full (job->files, g_object_unref);
       job->files = NULL;
+
+      /* we need to reload the files after the job is finished */
+      job->files = supported_files;
 
       /* we assume success if we've come so far */
       success = TRUE;
@@ -934,6 +934,9 @@ thunar_thumbnailer_thumbnailer_finished (GDBusProxy        *proxy,
           /* tell everybody we're done here */
           g_signal_emit (G_OBJECT (thumbnailer), thumbnailer_signals[REQUEST_FINISHED], 0, job->request);
 
+          for (GList *file_lp = job->files; file_lp != NULL; file_lp = file_lp->next)
+            thunar_file_reload (THUNAR_FILE (file_lp->data));
+
           /* remove job from the list */
           thumbnailer->jobs = g_slist_delete_link (thumbnailer->jobs, lp);
 
@@ -1031,13 +1034,13 @@ thunar_thumbnailer_idle_func (gpointer user_data)
             {
               /* set thumbnail state to none unless the thumbnail has already been created.
                * This is to prevent race conditions with the other idle functions */
-              if (thunar_file_get_thumb_state (file) != THUNAR_FILE_THUMB_STATE_READY)
-                thunar_file_set_thumb_state (file, THUNAR_FILE_THUMB_STATE_NONE);
+              if (thunar_file_get_thumb_state (file, idle->thumbnailer->thumbnail_size) != THUNAR_FILE_THUMB_STATE_READY)
+                thunar_file_set_thumb_state (file, THUNAR_FILE_THUMB_STATE_NONE, idle->thumbnailer->thumbnail_size);
             }
           else if (idle->type == THUNAR_THUMBNAILER_IDLE_READY)
             {
               /* set thumbnail state to ready - we now have a thumbnail */
-              thunar_file_set_thumb_state (file, THUNAR_FILE_THUMB_STATE_READY);
+              thunar_file_set_thumb_state (file, THUNAR_FILE_THUMB_STATE_READY, idle->thumbnailer->thumbnail_size);
             }
           else
             {

thunar/thunar-details-view.c
@@ -126,12 +126,9 @@ static void         thunar_details_view_append_menu_items       (ThunarStandardV
                                                                  GtkAccelGroup          *accel_group);
 static void         thunar_details_view_highlight_option_changed(ThunarDetailsView      *details_view);
 static void         thunar_details_view_queue_redraw            (ThunarStandardView     *standard_view);
-static gboolean     thunar_details_view_toggle_expandable_folders(ThunarDetailsView     *details_view);
-static void         thunar_details_view_finished_thumbnailing   (ThunarThumbnailer      *thumbnailer,
-                                                                 guint                   request,
-                                                                 ThunarDetailsView      *view);
-static void         thunar_details_view_block_selection_changed (ThunarStandardView *standard_view);
-static void         thunar_details_view_unblock_selection_changed (ThunarStandardView *standard_view);
+static gboolean     thunar_details_view_toggle_expandable_folders (ThunarDetailsView    *details_view);
+static void         thunar_details_view_block_selection_changed   (ThunarStandardView   *standard_view);
+static void         thunar_details_view_unblock_selection_changed (ThunarStandardView   *standard_view);
 
 
 
@@ -166,9 +163,6 @@ struct _ThunarDetailsView
   ExoTreeView       *tree_view;
 
   gboolean           expandable_folders;
-
-  ThunarThumbnailer *thumbnailer;
-  guint              thumbnail_request;
 };
 
 
@@ -411,14 +405,6 @@ thunar_details_view_init (ThunarDetailsView *details_view)
                             G_CALLBACK (thunar_details_view_highlight_option_changed), details_view);
   thunar_details_view_highlight_option_changed (details_view);
 
-  details_view->thumbnailer = thunar_thumbnailer_get ();
-  details_view->thumbnail_request = 0;
-
-  /* queue a redraw when thumbnail loading is finished */
-  g_signal_connect (details_view->thumbnailer, "request-finished",
-                    G_CALLBACK (thunar_details_view_finished_thumbnailing),
-                    details_view);
-
   /* release the shared text renderers */
   g_object_unref (G_OBJECT (right_aligned_renderer));
   g_object_unref (G_OBJECT (left_aligned_renderer));
@@ -497,9 +483,6 @@ thunar_details_view_finalize (GObject *object)
 
   g_signal_handlers_disconnect_by_func (G_OBJECT (THUNAR_STANDARD_VIEW (details_view)->preferences),
                                         thunar_details_view_highlight_option_changed, details_view);
-
-  g_signal_handlers_disconnect_by_func (G_OBJECT (details_view->thumbnailer),
-                                        thunar_details_view_finished_thumbnailing, details_view);
 
   (*G_OBJECT_CLASS (thunar_details_view_parent_class)->finalize) (object);
 }
@@ -1240,48 +1223,9 @@ thunar_details_view_row_expanded (GtkTreeView       *tree_view,
                                   ThunarDetailsView *view)
 {
   GtkTreeModel *model = gtk_tree_view_get_model (tree_view);
-  GtkTreeIter   child;
-  ThunarFile   *file = NULL;
-  GList        *files = NULL;
-  gboolean      has_next;
 
+  /* this will replace the dummy child with actual children */
   thunar_tree_view_model_load_subdir (THUNAR_TREE_VIEW_MODEL (model), parent);
-
-  /* do nothing if we are not supposed to show thumbnails at all */
-  if (!thunar_icon_factory_get_show_thumbnail (THUNAR_STANDARD_VIEW (view)->icon_factory,
-                                               thunar_navigator_get_current_directory (THUNAR_NAVIGATOR(view))))
-    return;
-
-  if (thunar_view_get_loading (THUNAR_VIEW (view)))
-    return;
-
-  has_next = gtk_tree_model_iter_children (model, &child, parent);
-
-  /* StandardView only requests thumbnails for direct descendants of
-   * currently active folder in view.
-   * Therefore thumbnail requests for newly visible files from a row
-   * expansion need to be made here separately. */
-
-  while (has_next)
-    {
-      /* can return NULL but shouldn't here since the rows are visible so the
-       * files should have been loaded by now */
-      file = thunar_standard_view_model_get_file (THUNAR_STANDARD_VIEW_MODEL (model), &child);
-      if (file != NULL)
-        files = g_list_prepend (files, file);
-      has_next = gtk_tree_model_iter_next (model, &child);
-    }
-
-  if (G_UNLIKELY (files == NULL))
-    return;
-
-  /* queue a thumbnail request */
-  thunar_thumbnailer_queue_files (view->thumbnailer,
-                                  TRUE, files, /* lazy: TRUE ? */
-                                  &view->thumbnail_request,
-                                  THUNAR_THUMBNAIL_SIZE_DEFAULT);
-
-  g_list_free_full (files, g_object_unref);
 }
 
 
@@ -1684,22 +1628,6 @@ thunar_details_view_toggle_expandable_folders (ThunarDetailsView *details_view)
   thunar_details_view_set_expandable_folders (details_view, !details_view->expandable_folders);
 
   return TRUE;
-}
-
-
-
-static void
-thunar_details_view_finished_thumbnailing (ThunarThumbnailer *thumbnailer,
-                                           guint              request,
-                                           ThunarDetailsView *view)
-{
-  _thunar_return_if_fail (THUNAR_IS_DETAILS_VIEW (view));
-
-  if (view->thumbnail_request == request)
-    {
-      view->thumbnail_request = 0;
-      thunar_details_view_queue_redraw (THUNAR_STANDARD_VIEW (view));
-    }
 }
 
 

thunar/thunar-enum-types.c
@@ -33,7 +33,6 @@ static void                thunar_icon_size_from_zoom_level     (const GValue   
 static void                thunar_thumbnail_size_from_icon_size (const GValue   *src_value,
                                                                  GValue         *dst_value);
 static ThunarIconSize      thunar_zoom_level_to_icon_size       (ThunarZoomLevel zoom_level);
-static ThunarThumbnailSize thunar_icon_size_to_thumbnail_size   (ThunarIconSize  icon_size);
 
 
 
@@ -523,7 +522,7 @@ thunar_zoom_level_to_view_margin (ThunarZoomLevel zoom_level)
 
 
 
-static ThunarThumbnailSize
+ThunarThumbnailSize
 thunar_icon_size_to_thumbnail_size (ThunarIconSize icon_size)
 {
   if (icon_size >= THUNAR_ICON_SIZE_1024)

thunar/thunar-enum-types.h
@@ -200,12 +200,13 @@ typedef enum
   THUNAR_THUMBNAIL_SIZE_LARGE,
   THUNAR_THUMBNAIL_SIZE_X_LARGE,
   THUNAR_THUMBNAIL_SIZE_XX_LARGE,
-
+  N_THUMBNAIL_SIZES,
   THUNAR_THUMBNAIL_SIZE_DEFAULT = -1
 } ThunarThumbnailSize;
 
-GType       thunar_thumbnail_size_get_type (void)                               G_GNUC_CONST;
-const char* thunar_thumbnail_size_get_nick (ThunarThumbnailSize thumbnail_size) G_GNUC_CONST;
+GType               thunar_thumbnail_size_get_type     (void)                               G_GNUC_CONST;
+const char*         thunar_thumbnail_size_get_nick     (ThunarThumbnailSize thumbnail_size) G_GNUC_CONST;
+ThunarThumbnailSize thunar_icon_size_to_thumbnail_size (ThunarIconSize      icon_size);
 
 
 #define THUNAR_TYPE_PARALLEL_COPY_MODE (thunar_parallel_copy_mode_get_type ())

thunar/thunar-file.c
@@ -69,6 +69,7 @@
 #include "thunar/thunar-private.h"
 #include "thunar/thunar-user.h"
 #include "thunar/thunar-util.h"
+#include "thunar/thunar-thumbnailer.h"
 
 
 
@@ -138,8 +139,6 @@ static guint              file_signals[LAST_SIGNAL];
 
 
 
-#define FLAG_SET_THUMB_STATE(file,new_state) G_STMT_START{ (file)->flags = ((file)->flags & ~THUNAR_FILE_FLAG_THUMB_MASK) | (new_state); }G_STMT_END
-#define FLAG_GET_THUMB_STATE(file)           ((file)->flags & THUNAR_FILE_FLAG_THUMB_MASK)
 #define FLAG_SET(file,flag)                  G_STMT_START{ ((file)->flags |= (flag)); }G_STMT_END
 #define FLAG_UNSET(file,flag)                G_STMT_START{ ((file)->flags &= ~(flag)); }G_STMT_END
 #define FLAG_IS_SET(file,flag)               (((file)->flags & (flag)) != 0)
@@ -180,7 +179,8 @@ struct _ThunarFile
   gchar                *display_name;
   gchar                *basename;
   const gchar          *device_type;
-  gchar                *thumbnail_path;
+  gchar                *thumbnail_path[N_THUMBNAIL_SIZES];
+  ThunarFileThumbState  thumbnail_state[N_THUMBNAIL_SIZES];
 
   /* sorting */
   gchar                *collate_key;
@@ -197,7 +197,6 @@ struct _ThunarFile
    * there were > 10.000 files in a folder (Creation of #ThunarFolder seems to be slow) */
   guint                 file_count;
   guint64               file_count_timestamp;
-
 };
 
 typedef struct
@@ -397,6 +396,11 @@ thunar_file_init (ThunarFile *file)
   file->file_count = 0;
   file->file_count_timestamp = 0;
   file->display_name = NULL;
+  for (gint i = 0; i < N_THUMBNAIL_SIZES; i++)
+    {
+      file->thumbnail_path[i] = NULL;
+      file->thumbnail_state[i] = THUNAR_FILE_THUMB_STATE_UNKNOWN;
+    }
 }
 
 
@@ -483,7 +487,11 @@ thunar_file_finalize (GObject *object)
   g_free (file->collate_key);
 
   /* free the thumbnail path */
-  g_free (file->thumbnail_path);
+  for (gint i = 0; i < N_THUMBNAIL_SIZES; i++)
+    {
+      g_free (file->thumbnail_path[i]);
+      file->thumbnail_path[i] = NULL;
+    }
 
   /* release file */
   g_object_unref (file->gfile);
@@ -606,7 +614,12 @@ thunar_file_info_changed (ThunarxFileInfo *file_info)
 
   /* set the new thumbnail state manually, so we only emit file
    * changed once */
-  FLAG_SET_THUMB_STATE (file, THUNAR_FILE_THUMB_STATE_UNKNOWN);
+  for (gint i = 0; i < N_THUMBNAIL_SIZES; i++)
+    {
+      file->thumbnail_state[i] = THUNAR_FILE_THUMB_STATE_UNKNOWN;
+      g_free (file->thumbnail_path[i]);
+      file->thumbnail_path[i] = NULL;
+    }
 
   /* tell the file monitor that this file changed */
   thunar_file_monitor_file_changed (file);
@@ -891,14 +904,18 @@ thunar_file_info_clear (ThunarFile *file)
   file->collate_key = NULL;
 
   /* free thumbnail path */
-  g_free (file->thumbnail_path);
-  file->thumbnail_path = NULL;
+  for (gint i = 0; i < N_THUMBNAIL_SIZES; i++)
+    {
+      g_free (file->thumbnail_path[i]);
+      file->thumbnail_path[i] = NULL;
+    }
 
   /* assume the file is mounted by default */
   FLAG_SET (file, THUNAR_FILE_FLAG_IS_MOUNTED);
 
   /* set thumb state to unknown */
-  FLAG_SET_THUMB_STATE (file, THUNAR_FILE_THUMB_STATE_UNKNOWN);
+  for (gint i = 0; i < N_THUMBNAIL_SIZES; i++)
+    file->thumbnail_state[i] = THUNAR_FILE_THUMB_STATE_UNKNOWN;
 }
 
 
@@ -3830,13 +3847,13 @@ thunar_file_get_thumbnail_path (ThunarFile *file, ThunarThumbnailSize thumbnail_
   _thunar_return_val_if_fail (THUNAR_IS_FILE (file), NULL);
 
   /* if the thumbstate is known to be not there, return null */
-  if (thunar_file_get_thumb_state (file) == THUNAR_FILE_THUMB_STATE_NONE)
+  if (thunar_file_get_thumb_state (file, thumbnail_size) == THUNAR_FILE_THUMB_STATE_NONE)
     return NULL;
 
-  if (G_UNLIKELY (file->thumbnail_path == NULL))
-    file->thumbnail_path = thunar_file_get_thumbnail_path_forced (file, thumbnail_size);
+  if (G_UNLIKELY (file->thumbnail_path[thumbnail_size] == NULL))
+    file->thumbnail_path[thumbnail_size] = thunar_file_get_thumbnail_path_forced (file, thumbnail_size);
 
-  return file->thumbnail_path;
+  return file->thumbnail_path[thumbnail_size];
 }
 
 
@@ -3916,6 +3933,7 @@ thunar_file_get_thumbnail_path_forced (ThunarFile *file, ThunarThumbnailSize thu
 /**
  * thunar_file_get_thumb_state:
  * @file : a #ThunarFile.
+ * @size : requested #ThunarThumbnailSize
  *
  * Returns the current #ThunarFileThumbState for @file. This
  * method is intended to be used by #ThunarIconFactory only.
@@ -3923,10 +3941,11 @@ thunar_file_get_thumbnail_path_forced (ThunarFile *file, ThunarThumbnailSize thu
  * Return value: the #ThunarFileThumbState for @file.
  **/
 ThunarFileThumbState
-thunar_file_get_thumb_state (const ThunarFile *file)
+thunar_file_get_thumb_state (const ThunarFile   *file,
+                             ThunarThumbnailSize size)
 {
   _thunar_return_val_if_fail (THUNAR_IS_FILE (file), THUNAR_FILE_THUMB_STATE_UNKNOWN);
-  return FLAG_GET_THUMB_STATE (file);
+  return file->thumbnail_state[size];
 }
 
 
@@ -3935,6 +3954,7 @@ thunar_file_get_thumb_state (const ThunarFile *file)
  * thunar_file_set_thumb_state:
  * @file        : a #ThunarFile.
  * @thumb_state : the new #ThunarFileThumbState.
+ * @thumb_size  : the required #ThunarThumbnailSize
  *
  * Sets the #ThunarFileThumbState for @file to @thumb_state.
  * This will cause a "file-changed" signal to be emitted from
@@ -3942,23 +3962,23 @@ thunar_file_get_thumb_state (const ThunarFile *file)
  **/
 void
 thunar_file_set_thumb_state (ThunarFile          *file,
-                             ThunarFileThumbState state)
+                             ThunarFileThumbState state,
+                             ThunarThumbnailSize  size)
 {
   _thunar_return_if_fail (THUNAR_IS_FILE (file));
 
   /* check if the state changes */
-  if (thunar_file_get_thumb_state (file) == state)
+  if (thunar_file_get_thumb_state (file, size) == state)
     return;
 
   /* set the new thumbnail state */
-  FLAG_SET_THUMB_STATE (file, state);
+  file->thumbnail_state[size] = state;
 
   /* remove path if the type is not supported */
-  if (state == THUNAR_FILE_THUMB_STATE_NONE
-      && file->thumbnail_path != NULL)
+  if (state == THUNAR_FILE_THUMB_STATE_NONE)
     {
-      g_free (file->thumbnail_path);
-      file->thumbnail_path = NULL;
+      g_free (file->thumbnail_path[size]);
+      file->thumbnail_path[size] = NULL;
     }
 
   /* if the file has a thumbnail, reload it */

thunar/thunar-file.h
@@ -272,9 +272,11 @@ const gchar     *thunar_file_get_thumbnail_path          (ThunarFile            
                                                           ThunarThumbnailSize      thumbnail_size);
 gchar           *thunar_file_get_thumbnail_path_forced   (ThunarFile              *file,
                                                           ThunarThumbnailSize      thumbnail_size);
-ThunarFileThumbState thunar_file_get_thumb_state         (const ThunarFile        *file);
-void             thunar_file_set_thumb_state             (ThunarFile              *file,
-                                                          ThunarFileThumbState     state);
+ThunarFileThumbState thunar_file_get_thumb_state         (const ThunarFile        *file,
+                                                          ThunarThumbnailSize      size);
+void                 thunar_file_set_thumb_state         (ThunarFile              *file,
+                                                          ThunarFileThumbState     state,
+                                                          ThunarThumbnailSize      size);
 GIcon            *thunar_file_get_preview_icon           (const ThunarFile        *file);
 const gchar      *thunar_file_get_icon_name              (ThunarFile              *file,
                                                           ThunarFileIconState      icon_state,

thunar/thunar-icon-factory.c
@@ -35,6 +35,7 @@
 #include "thunar/thunar-preferences.h"
 #include "thunar/thunar-private.h"
 #include "thunar/thunar-util.h"
+#include "thunar/thunar-thumbnailer.h"
 
 
 
@@ -125,6 +126,8 @@ struct _ThunarIconFactory
 
   /* stamp that gets bumped when the theme changes */
   guint                theme_stamp;
+
+  ThunarThumbnailer   *thumbnailer;
 };
 
 struct _ThunarIconKey
@@ -247,6 +250,8 @@ thunar_icon_factory_init (ThunarIconFactory *factory)
 {
   factory->thumbnail_mode = THUNAR_THUMBNAIL_MODE_ONLY_LOCAL;
   factory->thumbnail_size = THUNAR_THUMBNAIL_SIZE_NORMAL;
+
+  factory->thumbnailer = thunar_thumbnailer_get ();
 
   /* connect emission hook for the "changed" signal on the GtkIconTheme class. We use the emission
    * hook way here, because that way we can make sure that the icon cache is definetly cleared
@@ -920,6 +925,7 @@ thunar_icon_factory_load_file_icon (ThunarIconFactory  *factory,
   const gchar     *icon_name;
   const gchar     *custom_icon;
   ThunarIconStore *store;
+  guint            request;
 
   _thunar_return_val_if_fail (THUNAR_IS_ICON_FACTORY (factory), NULL);
   _thunar_return_val_if_fail (THUNAR_IS_FILE (file), NULL);
@@ -931,7 +937,7 @@ thunar_icon_factory_load_file_icon (ThunarIconFactory  *factory,
       && store->icon_state == icon_state
       && store->icon_size == icon_size
       && store->stamp == factory->theme_stamp
-      && store->thumb_state == thunar_file_get_thumb_state (file))
+      && store->thumb_state == thunar_file_get_thumb_state (file, thunar_icon_size_to_thumbnail_size (icon_size)))
     {
       return g_object_ref (store->icon);
     }
@@ -1004,14 +1010,15 @@ thunar_icon_factory_load_file_icon (ThunarIconFactory  *factory,
             {
               /* we have no preview icon but the thumbnail should be ready. determine
                * the filename of the thumbnail */
-              thumbnail_path = thunar_file_get_thumbnail_path (file, factory->thumbnail_size);
+              thumbnail_path = thunar_file_get_thumbnail_path (file, thunar_icon_size_to_thumbnail_size (icon_size));
 
               /* check if we have a valid path */
               if (thumbnail_path != NULL)
-                {
-                  /* try to load the thumbnail */
-                  icon = thunar_icon_factory_load_from_file (factory, thumbnail_path, icon_size, scale_factor);
-                }
+                /* try to load the thumbnail */
+                icon = thunar_icon_factory_load_from_file (factory, thumbnail_path, icon_size, scale_factor);
+              else if (thunar_file_get_thumb_state (file, thunar_icon_size_to_thumbnail_size (icon_size)) != THUNAR_FILE_THUMB_STATE_NONE)
+                /* thumbnail does not exist; so load it */
+                thunar_thumbnailer_queue_file (factory->thumbnailer, file, &request, thunar_icon_size_to_thumbnail_size (icon_size));
             }
         }
     }
@@ -1029,7 +1036,7 @@ thunar_icon_factory_load_file_icon (ThunarIconFactory  *factory,
       store->icon_size = icon_size;
       store->icon_state = icon_state;
       store->stamp = factory->theme_stamp;
-      store->thumb_state = thunar_file_get_thumb_state (file);
+      store->thumb_state = thunar_file_get_thumb_state (file, thunar_icon_size_to_thumbnail_size (icon_size));
       store->icon = g_object_ref (icon);
 
       g_object_set_qdata_full (G_OBJECT (file), thunar_icon_factory_store_quark,

commit ceb5fba92b73366d86e4c51754752370c18ae444

Prevent deadlock during thumbnail loading (#1217)
Fixes a regression introduced by 6515c587

thunar/thunar-thumbnailer.c
@@ -147,8 +147,6 @@ static void                   thunar_thumbnailer_set_property           (GObject
 
 #define _thumbnailer_lock(thumbnailer)    g_mutex_lock (&((thumbnailer)->lock))
 #define _thumbnailer_unlock(thumbnailer)  g_mutex_unlock (&((thumbnailer)->lock))
-#define _thumbnailer_trylock(thumbnailer) g_mutex_trylock (&((thumbnailer)->lock))
-
 
 
 struct _ThunarThumbnailerClass
@@ -910,6 +908,7 @@ thunar_thumbnailer_thumbnailer_finished (GDBusProxy        *proxy,
 {
   ThunarThumbnailerJob *job;
   GSList               *lp;
+  GList                *files_to_reload = NULL;
 
   _thunar_return_if_fail (G_IS_DBUS_PROXY (proxy));
   _thunar_return_if_fail (THUNAR_IS_THUMBNAILER (thumbnailer));
@@ -935,7 +934,7 @@ thunar_thumbnailer_thumbnailer_finished (GDBusProxy        *proxy,
           g_signal_emit (G_OBJECT (thumbnailer), thumbnailer_signals[REQUEST_FINISHED], 0, job->request);
 
           for (GList *file_lp = job->files; file_lp != NULL; file_lp = file_lp->next)
-            thunar_file_reload (THUNAR_FILE (file_lp->data));
+            files_to_reload = g_list_append (files_to_reload, g_object_ref (file_lp->data));
 
           /* remove job from the list */
           thumbnailer->jobs = g_slist_delete_link (thumbnailer->jobs, lp);
@@ -946,6 +945,13 @@ thunar_thumbnailer_thumbnailer_finished (GDBusProxy        *proxy,
     }
 
   _thumbnailer_unlock (thumbnailer);
+
+  /* Do the reload outside of teh critical section in order to prevent a deadlock */
+  for (GList *file_lp = files_to_reload; file_lp != NULL; file_lp = file_lp->next)
+    thunar_file_reload (THUNAR_FILE (file_lp->data));
+  
+  if (files_to_reload != NULL)
+    g_list_free_full (files_to_reload, g_object_unref);
 }
 
 