xfce-mirror/thunar

commit 376bc4a998f1423b9e86fe4010125e68b8bac2de

Properly release job when finished (#1192)
Conetnt type job was not properly released, leading do leaked ThunarFIle objects in somecases  .. see commit da573a80

thunar/thunar-folder.c
@@ -690,6 +690,18 @@ thunar_folder_files_ready (ThunarJob    *job,
 
 
 
+static void
+_thunar_folder_load_content_types_finished (ThunarFolder *folder, ThunarJob* job)
+{
+  if (folder->content_type_job != NULL)
+    {
+      g_object_unref (folder->content_type_job);
+      folder->content_type_job = NULL;
+    }
+}
+
+
+
 /**
  * thunar_folder_load_content_types:
  * @folder : a #ThunarFolder instance.
@@ -713,6 +725,8 @@ thunar_folder_load_content_types (ThunarFolder *folder,
 
   /* start a new content_type_job */
   folder->content_type_job = thunar_io_jobs_load_content_types (files);
+  g_signal_connect_swapped (folder->content_type_job, "finished", G_CALLBACK (_thunar_folder_load_content_types_finished), folder);
+
   exo_job_launch (EXO_JOB (folder->content_type_job));
 }
 

commit bbfa5e799f5d009f2d5f0c09e6bece5e511e29c5

Fix possible double-free on thunar job (#1299)
Regression introduced in 376bc4a9

thunar/thunar-folder.c
@@ -717,11 +717,7 @@ thunar_folder_load_content_types (ThunarFolder *folder,
 
   /* check if we are currently connect to a job */
   if (G_UNLIKELY (folder->content_type_job != NULL))
-    {
       exo_job_cancel (EXO_JOB (folder->content_type_job));
-      g_object_unref (folder->content_type_job);
-      folder->content_type_job = NULL;
-    }
 
   /* start a new content_type_job */
   folder->content_type_job = thunar_io_jobs_load_content_types (files);
@@ -1258,11 +1254,7 @@ thunar_folder_reload (ThunarFolder *folder,
 
   /* stop content type loading */
   if (G_UNLIKELY (folder->content_type_job != NULL))
-    {
-      exo_job_cancel (EXO_JOB (folder->content_type_job));
-      g_object_unref (folder->content_type_job);
-      folder->content_type_job = NULL;
-    }
+    exo_job_cancel (EXO_JOB (folder->content_type_job));
 
   /* check if we are currently connect to a job */
   if (G_UNLIKELY (folder->job != NULL))