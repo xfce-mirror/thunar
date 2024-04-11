xfce-mirror/thunar

commit 958e97ebbde7a4a735f8e35c0030d1a72fae8eab

Improve icon view performance (#1315)
In 'thunar_list_model_files_changed' it makes sense to assume that the number of changed files is smaller than the number of files which are already in the model. Otherwise a a folder with many files inside will iterate on them again and again for each added file

thunar/thunar-list-model.c
@@ -1398,24 +1398,22 @@ thunar_list_model_files_changed (ThunarFolder    *folder,
   gint           i, j;
   GtkTreePath   *path;
   GtkTreeIter    iter;
-  gboolean       found = FALSE;
   GList          node, *lp;
   GSList        *hidden_link = NULL;
 
   _thunar_return_if_fail (THUNAR_IS_LIST_MODEL (store));
 
-  for (lp = files; lp != NULL; lp = lp->next)
+  row = g_sequence_get_begin_iter (store->rows);
+  end = g_sequence_get_end_iter (store->rows);
+  while (row != end)
     {
-      file = lp->data;
-      row = g_sequence_get_begin_iter (store->rows);
-      end = g_sequence_get_end_iter (store->rows);
+      pos_before = g_sequence_iter_get_position (row);
 
-      while (row != end)
+      for (lp = files; lp != NULL; lp = lp->next)
         {
-          pos_before = g_sequence_iter_get_position (row);
+          file = lp->data;
           if (G_UNLIKELY (g_sequence_get (row) == file))
             {
-              found = TRUE;
               /* this file is hidden now & show_hidden is FALSE
               * so we should remove this file from the view and store
               * it in the hidden list */
@@ -1483,26 +1481,24 @@ thunar_list_model_files_changed (ThunarFolder    *folder,
               break;
             }
 
-          row = g_sequence_iter_next (row);
-          pos_before++;
-        } /* end while loop */
+          /* maybe this file was a hidden file but now it's not
+          * in such a case we need to emit a "files-added" for this file
+          * and remove it from the hidden list */
+          hidden_link = g_slist_find (store->hidden, file);
+          if (hidden_link == NULL || thunar_file_is_hidden (file))
+            continue;
+          g_object_unref (hidden_link->data);
+          store->hidden = g_slist_delete_link (store->hidden, hidden_link);
+          node.data = file;
+          node.next = NULL;
+          node.prev = NULL;
+          thunar_list_model_files_added (store->folder, &node, store);
 
-      if (found)
-        continue;
+        } /* end for each file loop */
 
-      /* maybe this file was a hidden file but now it's not
-      * in such a case we need to emit a "files-added" for this file
-      * and remove it from the hidden list */
-      hidden_link = g_slist_find (store->hidden, file);
-      if (hidden_link == NULL || thunar_file_is_hidden (file))
-        continue;
-      g_object_unref (hidden_link->data);
-      store->hidden = g_slist_delete_link (store->hidden, hidden_link);
-      node.data = file;
-      node.next = NULL;
-      node.prev = NULL;
-      thunar_list_model_files_added (store->folder, &node, store);
-   } /* end for loop */
+      row = g_sequence_iter_next (row);
+      pos_before++;
+   } /* end while loop (for each row) */
 }
 
 
@@ -1662,7 +1658,7 @@ thunar_list_model_insert_files (ThunarListModel *store,
               GTK_TREE_ITER_INIT (iter, store->stamp, row);
 
               indices[0] = g_sequence_iter_get_position (row);
-              gtk_tree_model_row_inserted (GTK_TREE_MODEL (store), path, &iter);
+              //gtk_tree_model_row_inserted (GTK_TREE_MODEL (store), path, &iter);
             }
         }
     }

commit 17791bd15f1db8dd5766fd7516fbf462fd1de1a7

Fix icon view regression (#1315)
Bug introduced in  958e97eb

thunar/thunar-list-model.c
@@ -1407,13 +1407,13 @@ thunar_list_model_files_changed (ThunarFolder    *folder,
   end = g_sequence_get_end_iter (store->rows);
   while (row != end)
     {
-      pos_before = g_sequence_iter_get_position (row);
-
       for (lp = files; lp != NULL; lp = lp->next)
         {
           file = lp->data;
           if (G_UNLIKELY (g_sequence_get (row) == file))
             {
+              pos_before = g_sequence_iter_get_position (row);
+
               /* this file is hidden now & show_hidden is FALSE
               * so we should remove this file from the view and store
               * it in the hidden list */
@@ -1433,7 +1433,6 @@ thunar_list_model_files_changed (ThunarFolder    *folder,
               GTK_TREE_ITER_INIT (iter, store->stamp, row);
               
               /* check if the sorting changed */
-              pos_before = g_sequence_iter_get_position (row);
               g_sequence_sort_changed (row, thunar_list_model_cmp_func, store);
               pos_after = g_sequence_iter_get_position (row);
 
@@ -1473,12 +1472,13 @@ thunar_list_model_files_changed (ThunarFolder    *folder,
 
                   store->file_was_sorted = TRUE;
                 }
-
-              /* notify the view that it has to redraw the file */
-              path = gtk_tree_path_new_from_indices (pos_before, -1);
-              gtk_tree_model_row_changed (GTK_TREE_MODEL (store), path, &iter);
-              gtk_tree_path_free (path);
-              break;
+              else
+                {
+                  /* just notify the view that it has to redraw the file */
+                  path = gtk_tree_path_new_from_indices (pos_before, -1);
+                  gtk_tree_model_row_changed (GTK_TREE_MODEL (store), path, &iter);//this line 
+                  gtk_tree_path_free (path);
+                }
             }
 
           /* maybe this file was a hidden file but now it's not
@@ -1497,7 +1497,6 @@ thunar_list_model_files_changed (ThunarFolder    *folder,
         } /* end for each file loop */
 
       row = g_sequence_iter_next (row);
-      pos_before++;
    } /* end while loop (for each row) */
 }
 
@@ -1658,7 +1657,7 @@ thunar_list_model_insert_files (ThunarListModel *store,
               GTK_TREE_ITER_INIT (iter, store->stamp, row);
 
               indices[0] = g_sequence_iter_get_position (row);
-              //gtk_tree_model_row_inserted (GTK_TREE_MODEL (store), path, &iter);
+              gtk_tree_model_row_inserted (GTK_TREE_MODEL (store), path, &iter);
             }
         }
     }