ee8c19291862c1ae087d487dcc2b9df64189ed84
Theo Linkspfeifer <lastonestanding@tutanota.com>
2024-08-23 13:16:16 +0200

    Fix handling of special columns in list view
    - Make the special columns non mutable in the column editor
    - Ensure that these columns are only visible for specific locations
    - Exclude them when saving the list of visible columns
    
    Fixes: #791
    Fixes: #894
    Fixes: #1201

--- thunar/thunar-enum-types.h
+++ thunar/thunar-enum-types.h
@@ -138,8 +138,9 @@ typedef enum
 
 GType        thunar_column_get_type          (void)                      G_GNUC_CONST;
 const gchar* thunar_column_string_from_value (ThunarColumn  value);
-gboolean     thunar_column_value_from_string (const gchar   *value_string,
-                                              ThunarColumn  *value);
+gboolean     thunar_column_value_from_string (const gchar  *value_string,
+                                              ThunarColumn *value);
+gboolean     thunar_column_is_special        (ThunarColumn  value);
 
 
 #define THUNAR_TYPE_ICON_SIZE (thunar_icon_size_get_type ())
