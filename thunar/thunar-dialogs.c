f33a6650def379f8d4fdc67a5452f7a2392050ff
Zetta1  Reid0 <11255-Zetta1_Reid0@users.noreply.gitlab.xfce.org>
2024-08-28 12:55:32 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Theo Linkspfeifer <lastonestanding@tutanota.com>

--- thunar/thunar-dialogs.c
+++ thunar/thunar-dialogs.c
@@ -820,7 +820,7 @@ thunar_dialogs_show_job_ask_replace (GtkWindow  *parent,
 
   /* setup the confirmation dialog */
   dialog = xfce_titled_dialog_new ();
-  gtk_window_set_resizable (GTK_WINDOW (dialog) , FALSE);
+  gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
   gtk_window_set_title (GTK_WINDOW (dialog), _("Confirm to replace files"));
   gtk_window_set_transient_for (GTK_WINDOW (dialog), parent);
   gtk_window_set_destroy_with_parent (GTK_WINDOW (dialog), TRUE);
