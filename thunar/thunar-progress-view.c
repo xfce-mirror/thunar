856ff220f613717fc613c49f4562f0c2716e6b3f
Zetta1  Reid0 <11255-Zetta1_Reid0@users.noreply.gitlab.xfce.org>
2024-08-28 12:57:51 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Theo Linkspfeifer <lastonestanding@tutanota.com>

--- thunar/thunar-progress-view.c
+++ thunar/thunar-progress-view.c
@@ -510,11 +510,8 @@ thunar_progress_view_ask_replace (ThunarProgressView *view,
   n_total_files = thunar_job_get_n_total_files (job);
   view->n_asks_answered++;
 
-  if (n_total_files > 1)
-      multiple_files = TRUE;
-
-  if (n_total_files == view->n_asks_answered)
-      multiple_files = FALSE;
+  if (n_total_files > 1 && n_total_files != view->n_asks_answered)
+    multiple_files = TRUE;
 
   /* display the question view */
   return thunar_dialogs_show_job_ask_replace (window != NULL ? GTK_WINDOW (window) : NULL,
