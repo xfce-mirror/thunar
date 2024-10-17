3828d868a892359cf71a7b58d05137c7b66bd758
Brian Tarricone <brian@tarricone.org>
2024-10-17 19:47:14 +0000

    Verify by compare instead of checksum (#1471)
    Since we have to read both the source (again) and destination files in
    order to verify them, there's no reason to calculate a checksum: we can
    simply compare the file contents with each other.
    
    Even faster might be to stop using g_file_copy(), and instead "manually"
    copy the file so we can feed the source bytes into a checksum generator
    on the fly, without having to re-read the source file again at the end.
    We could even chunk the file and generate a checksum per chunk in
    parallel on different threads, and then compare the chunk checksums at
    the end.
    
    But this should give us a nice speedup, and is much, much simpler.
    
    Closes #1471
    MR xfce/thunar!557

--- thunar/thunar-preferences-dialog.c
+++ thunar/thunar-preferences-dialog.c
@@ -1168,13 +1168,13 @@ thunar_preferences_dialog_init (ThunarPreferencesDialog *dialog)
   /* next row */
   row++;
 
-  label = gtk_label_new_with_mnemonic (_("Verify file checksum on copy:"));
+  label = gtk_label_new_with_mnemonic (_("Verify file contents on copy:"));
   gtk_label_set_xalign (GTK_LABEL (label), 0.0f);
   gtk_grid_attach (GTK_GRID (grid), label, 0, row, 1, 1);
   gtk_widget_show (label);
-  gtk_widget_set_tooltip_text (label, _("Compare original file and copy with checksum. "
+  gtk_widget_set_tooltip_text (label, _("Compare contents of original file and copy. "
                                         "This will prevent corrupted files. "
-                                        "Enabling this option will largely increase copy time."));
+                                        "Enabling this option will slightly increase copy time."));
 
   combo = gtk_combo_box_text_new ();
   type = g_type_class_ref (THUNAR_TYPE_VERIFY_FILE_MODE);
