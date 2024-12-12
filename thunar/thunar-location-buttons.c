xfce-mirror/thunar

commit 9d9ce5ac61fcb43c1db6a57e46bd75afb7f1e23e

Rework handling of location buttons (#951)
- Try to avoid hiding the active button when resizing the window
- Dynamically calculate the maximum button width

Fixes: #951

thunar/thunar-location-buttons.c
@@ -40,7 +40,9 @@
 
 
 
-#define THUNAR_LOCATION_BUTTONS_SCROLL_TIMEOUT  (200)
+#define THUNAR_LOCATION_BUTTON_MAX_WIDTH 250
+#define THUNAR_LOCATION_BUTTON_FACTOR 0.9
+#define THUNAR_LOCATION_BUTTONS_SCROLL_TIMEOUT 200
 
 
 
@@ -138,16 +140,18 @@ struct _ThunarLocationButtons
 {
   GtkContainer __parent__;
 
+  ThunarActionManager *action_mgr;
+
   GtkWidget           *left_slider;
   GtkWidget           *right_slider;
   GtkWidget           *filler_widget;
 
   ThunarFile          *current_directory;
 
+  gint                 button_max_width;
   gint                 slider_width;
   guint                ignore_click : 1;
 
-  ThunarActionManager *action_mgr;
   GList               *list;
   GList               *fake_root_button;
   GList               *first_visible_button;
@@ -243,6 +247,8 @@ thunar_location_buttons_init (ThunarLocationButtons *buttons)
 
   gtk_widget_set_has_window (GTK_WIDGET (buttons), FALSE);
   gtk_widget_set_redraw_on_allocate (GTK_WIDGET (buttons), FALSE);
+
+  buttons->button_max_width = THUNAR_LOCATION_BUTTON_MAX_WIDTH;
 
   buttons->left_slider = gtk_button_new ();
   g_signal_connect (G_OBJECT (buttons->left_slider), "button-press-event", G_CALLBACK (thunar_location_buttons_slider_button_press), buttons);
@@ -624,6 +630,10 @@ thunar_location_buttons_detect_first_visible_button (ThunarLocationButtons *butt
       resulting_width += button_width;
       if(resulting_occupied_width != NULL)
         *resulting_occupied_width = resulting_width;
+
+      /* stop at the fake root button */
+      if (lp == buttons->fake_root_button)
+        return lp;
     }
   return g_list_last (buttons->list);
 }
@@ -668,12 +678,15 @@ thunar_location_buttons_size_allocate (GtkWidget     *widget,
   GtkAllocation          second_slider_allocation; /* right slider if direction is LTR */
   GtkAllocation          filler_allocation;
   GList                 *lp;
-  gint                   required_width_total = 0;
+  gint                   sliders_filler_width;
+  gint                   buttons_area_width;
+  gint                   buttons_total_width;
   gint                   border_width;
   gint                   temp_width;
   gboolean               need_reorder = FALSE;
 
   gtk_widget_set_allocation (widget, allocation);
+
   /* if no path is set, we don't have to allocate anything */
   if (G_UNLIKELY (buttons->list == NULL))
     return;
@@ -683,7 +696,7 @@ thunar_location_buttons_size_allocate (GtkWidget     *widget,
   available_space.x = allocation->x + border_width;
   available_space.y = allocation->y + border_width;
   available_space.width = allocation->width - 2 * border_width;
-  available_space.height = MAX (1, allocation->height - 2 * border_width );
+  available_space.height = MAX (1, allocation->height - 2 * border_width);
 
   first_slider_allocation.width = buttons->slider_width;
   first_slider_allocation.height = available_space.height;
@@ -705,19 +718,36 @@ thunar_location_buttons_size_allocate (GtkWidget     *widget,
     }
 
   gtk_widget_get_preferred_width (buttons->filler_widget, &filler_allocation.width, NULL);
-  required_width_total += buttons->slider_width * 2 + filler_allocation.width;
   filler_allocation.height = available_space.height;
   filler_allocation.y = available_space.y;
 
+  /* space used by sliders + filler */
+  sliders_filler_width = buttons->slider_width * 2 + filler_allocation.width;
+
+  /* space available for buttons */
+  buttons_area_width = available_space.width - sliders_filler_width;
+
+  /* dynamically calculate the maximum button width relative to the available space
+   * by using a factor between 0 and 1 to allow the button to shrink in size */
+  buttons->button_max_width = MIN (buttons_area_width * THUNAR_LOCATION_BUTTON_FACTOR,
+                                   THUNAR_LOCATION_BUTTON_MAX_WIDTH);
+
+  /* apply the value to all buttons */
+  for (lp = buttons->list; lp != NULL; lp = lp->next)
+    thunar_location_button_set_max_width (THUNAR_LOCATION_BUTTON (lp->data), buttons->button_max_width);
+
+  /* calculate the total width of all buttons */
+  buttons_total_width = 0;
   for (lp = buttons->list; lp != NULL; lp = lp->next)
     {
       gtk_widget_get_preferred_width (GTK_WIDGET (lp->data), &temp_width, NULL);
-      required_width_total += temp_width;
+      buttons_total_width += temp_width;
       if (lp == buttons->fake_root_button)
         break;
     }
 
-  if ( required_width_total <= available_space.width  )
+  /* determine how many buttons fit into the available area */
+  if (buttons_total_width <= buttons_area_width)
     {
       /* Show full list of buttons, starting by fake root, if available */
       if (buttons->fake_root_button == NULL)
@@ -728,24 +758,29 @@ thunar_location_buttons_size_allocate (GtkWidget     *widget,
     }
   else
     {
-      /* reset calculation for required_width_total .. we need to cut off some folders */
-      required_width_total = first_slider_allocation.width + second_slider_allocation.width + filler_allocation.width;
+      /* reset calculation for buttons_total_width, because we need to cut off some buttons */
+      buttons_total_width = 0;
 
       /* No scrolling defined ? Use last button as fixed start! */
-      if(buttons->last_visible_button == NULL && buttons->first_visible_button == NULL)
+      if (buttons->last_visible_button == NULL && buttons->first_visible_button == NULL)
         buttons->last_visible_button = buttons->list;
 
-      if(buttons->first_visible_button == NULL) /* last button is fixed defined */
-        buttons->first_visible_button = thunar_location_buttons_detect_first_visible_button(buttons, available_space.width - required_width_total, &temp_width);
-      else                                      /* first button is fixed defined */
-        buttons->last_visible_button = thunar_location_buttons_detect_last_visible_button(buttons, available_space.width - required_width_total, &temp_width);
-
-      required_width_total += temp_width;
+      /* if the last visible button is the active one, do not cut it off */
+      if (buttons->last_visible_button != NULL
+          && gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (buttons->last_visible_button->data)))
+        {
+          buttons->first_visible_button = thunar_location_buttons_detect_first_visible_button (buttons, buttons_area_width, NULL);
+          buttons->last_visible_button = thunar_location_buttons_detect_last_visible_button (buttons, buttons_area_width, &buttons_total_width);
+        }
+      else if (buttons->first_visible_button == NULL) /* last button is fixed defined */
+        buttons->first_visible_button = thunar_location_buttons_detect_first_visible_button (buttons, buttons_area_width, &buttons_total_width);
+      else                                            /* first button is fixed defined */
+        buttons->last_visible_button = thunar_location_buttons_detect_last_visible_button (buttons, buttons_area_width, &buttons_total_width);
     }
 
+  /* hide buttons before first_visible_button */
   if (buttons->first_visible_button != NULL)
     {
-      /* hide buttons before first_visible_button */
       for (lp = buttons->first_visible_button->next; lp != NULL; lp = lp->next)
         {
           need_reorder |= gtk_widget_get_child_visible (GTK_WIDGET (lp->data)) == TRUE;
@@ -753,9 +788,9 @@ thunar_location_buttons_size_allocate (GtkWidget     *widget,
         }
     }
 
+  /* hide buttons after last visible_button */
   if (buttons->last_visible_button != NULL)
     {
-      /* hide buttons after last visible_button */
       for (lp = buttons->last_visible_button->prev; lp != NULL; lp = lp->prev)
         {
           need_reorder |= gtk_widget_get_child_visible (GTK_WIDGET (lp->data)) == TRUE;
@@ -790,20 +825,20 @@ thunar_location_buttons_size_allocate (GtkWidget     *widget,
       if (G_LIKELY (direction == GTK_TEXT_DIR_LTR))
         folder_button_allocation.x += folder_button_allocation.width;
 
-      if( lp == buttons->last_visible_button )
+      if (lp == buttons->last_visible_button)
         break;
     }
 
   /* allocate the filler */
-  filler_allocation.width += available_space.width - required_width_total;
+  filler_allocation.width += available_space.width - sliders_filler_width - buttons_total_width;
   filler_allocation.x = folder_button_allocation.x;
   if (G_UNLIKELY (direction == GTK_TEXT_DIR_RTL))
     filler_allocation.x -= filler_allocation.width;
-  gtk_widget_size_allocate (GTK_WIDGET (buttons->filler_widget), &filler_allocation);
+  gtk_widget_size_allocate (buttons->filler_widget, &filler_allocation);
 
   /* allocate the sliders */
-  gtk_widget_get_preferred_width (GTK_WIDGET (buttons->left_slider), &temp_width, NULL); //to dont get gtk warnings
-  gtk_widget_get_preferred_width (GTK_WIDGET (buttons->right_slider), &temp_width, NULL);
+  gtk_widget_get_preferred_width (GTK_WIDGET (buttons->left_slider), &temp_width, NULL); /* to avoid warnings in gtk >= 3.20 */
+  gtk_widget_get_preferred_width (GTK_WIDGET (buttons->right_slider), &temp_width, NULL); /* to avoid warnings in gtk >= 3.20 */
   gtk_widget_size_allocate (buttons->left_slider, &first_slider_allocation);
   gtk_widget_size_allocate (buttons->right_slider, &second_slider_allocation);
   thunar_location_buttons_update_sliders (buttons);
@@ -940,7 +975,7 @@ thunar_location_buttons_make_button (ThunarLocationButtons *buttons,
   GtkWidget *button;
 
   /* allocate the button */
-  button = thunar_location_button_new ();
+  button = g_object_new (THUNAR_TYPE_LOCATION_BUTTON, "max-width", buttons->button_max_width, NULL);
 
   /* connect to the file */
   thunar_location_button_set_file (THUNAR_LOCATION_BUTTON (button), file);

thunar/thunar-location-button.c
@@ -42,15 +42,12 @@
 
 
 
-#define THUNAR_LOCATION_BUTTON_MAX_WIDTH 250
-
-
-
 /* Property identifiers */
 enum
 {
   PROP_0,
   PROP_FILE,
+  PROP_MAX_WIDTH,
 };
 
 /* Signal identifiers */
@@ -145,6 +142,7 @@ struct _ThunarLocationButton
 
   /* public properties */
   ThunarFile         *file;
+  gint                max_width;
 };
 
 
@@ -189,6 +187,21 @@ thunar_location_button_class_init (ThunarLocationButtonClass *klass)
                                                         "file",
                                                         THUNAR_TYPE_FILE,
                                                         EXO_PARAM_READWRITE));
+
+  /**
+   * ThunarLocationButton:max-width:
+   *
+   * The desired maximum width of the button, in pixels. If this property
+   * is set to -1, the width will be calculated automatically.
+   **/
+  g_object_class_install_property (gobject_class,
+                                   PROP_MAX_WIDTH,
+                                   g_param_spec_int ("max-width",
+                                                     "max-width",
+                                                     "max-width",
+                                                     -1, G_MAXINT,
+                                                     -1,
+                                                     EXO_PARAM_READWRITE));
 
   /**
    * ThunarLocationButton::location-button-clicked:
@@ -308,6 +321,10 @@ thunar_location_button_get_property (GObject    *object,
       g_value_set_object (value, thunar_location_button_get_file (location_button));
       break;
 
+    case PROP_MAX_WIDTH:
+      g_value_set_int (value, thunar_location_button_get_max_width (location_button));
+      break;
+
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -328,6 +345,10 @@ thunar_location_button_set_property (GObject      *object,
     {
     case PROP_FILE:
       thunar_location_button_set_file (location_button, g_value_get_object (value));
+      break;
+
+    case PROP_MAX_WIDTH:
+      thunar_location_button_set_max_width (location_button, g_value_get_int (value));
       break;
 
     default:
@@ -415,6 +436,9 @@ thunar_location_button_file_changed (ThunarLocationButton *location_button,
       if (diplay_name != NULL)
         gtk_label_set_text (GTK_LABEL (location_button->label), diplay_name);
 
+      /* reset the size request before applying the new label size so that the button can shrink */
+      gtk_widget_set_size_request (location_button->label, -1, -1);
+
       /* set the label's size request in such a way that a bold label will not change the button size */
       if (gtk_widget_get_mapped (GTK_WIDGET (location_button)))
         thunar_location_button_apply_label_size (location_button);
@@ -457,10 +481,6 @@ thunar_location_button_file_changed (ThunarLocationButton *location_button,
       dnd_icon_name = thunar_file_get_icon_name (file, location_button->file_icon_state, icon_theme);
       gtk_drag_source_set_icon_name (GTK_WIDGET (location_button), dnd_icon_name);
     }
-
-  /* recalculate the required size in case the filename changed */
-  gtk_widget_set_size_request (GTK_WIDGET (location_button->label), -1, -1);
-
 }
 
 
@@ -503,11 +523,11 @@ thunar_location_button_apply_label_size (ThunarLocationButton *location_button)
   width = MAX (normal_size.width, bold_size.width);
   height = MAX (normal_size.height, bold_size.height);
 
-  if (width >= THUNAR_LOCATION_BUTTON_MAX_WIDTH)
+  if (width > location_button->max_width)
     {
       /* if the size is too big, enable ellipsizing */
       gtk_label_set_ellipsize (GTK_LABEL (location_button->label), PANGO_ELLIPSIZE_MIDDLE);
-      gtk_widget_set_size_request (GTK_WIDGET (location_button->label), THUNAR_LOCATION_BUTTON_MAX_WIDTH, height);
+      gtk_widget_set_size_request (location_button->label, location_button->max_width, height);
 
       /* and set a tooltip */
       gtk_widget_set_tooltip_text (location_button->label, text);
@@ -517,7 +537,7 @@ thunar_location_button_apply_label_size (ThunarLocationButton *location_button)
       /* don't enable ellipsizing: In some borderline cases, the size calculated
        * is actually off by 1 or 2 pixels and the label will ellipsize unnecessarily
        * TODO: check what we did wrong */
-      gtk_widget_set_size_request (GTK_WIDGET (location_button->label), width, height);
+      gtk_widget_set_size_request (location_button->label, width, height);
       gtk_widget_set_tooltip_text (location_button->label, NULL);
     }
 }
@@ -944,6 +964,51 @@ thunar_location_button_set_file (ThunarLocationButton *location_button,
 
   /* notify listeners */
   g_object_notify (G_OBJECT (location_button), "file");
+}
+
+
+
+/**
+ * thunar_location_button_get_max_width:
+ * @location_button : a #ThunarLocationButton.
+ *
+ * Retrieves the maximum width of @location_button, in pixels.
+ *
+ * Return value: the maximum width of @location_button in pixels.
+ **/
+gint
+thunar_location_button_get_max_width (ThunarLocationButton *location_button)
+{
+  _thunar_return_val_if_fail (THUNAR_IS_LOCATION_BUTTON (location_button), -1);
+
+  return location_button->max_width;
+}
+
+
+
+/**
+ * thunar_location_button_set_max_width:
+ * @location_button : a #ThunarLocationButton.
+ * @max_width       : the new maximum width, in pixels.
+ *
+ * Sets the maximum width in pixels of @location_button to @max_width.
+ **/
+void
+thunar_location_button_set_max_width (ThunarLocationButton *location_button,
+                                      gint                  max_width)
+{
+  _thunar_return_if_fail (THUNAR_IS_LOCATION_BUTTON (location_button));
+
+  if (location_button->max_width != max_width)
+    {
+      location_button->max_width = max_width;
+
+      /* update the label size */
+      thunar_location_button_apply_label_size (location_button);
+
+      /* notify listeners */
+      g_object_notify (G_OBJECT (location_button), "max-width");
+    }
 }
 
 

thunar/thunar-location-button.h
@@ -41,6 +41,9 @@ GtkWidget  *thunar_location_button_new        (void) G_GNUC_MALLOC;
 ThunarFile *thunar_location_button_get_file   (ThunarLocationButton *location_button);
 void        thunar_location_button_set_file   (ThunarLocationButton *location_button,
                                                ThunarFile           *file);
+gint        thunar_location_button_get_max_width (ThunarLocationButton *location_button);
+void        thunar_location_button_set_max_width (ThunarLocationButton *location_button,
+                                                  gint                  max_width);
 
 G_END_DECLS;
 

commit 69271d562fb03a6d9f7b973ffbd654671fee135c

Prevent crash when removing location button (#1507)
Clear the last_visible_button pointer whenever a folder is deleted and
its location button is removed.

Fixes: 9d9ce5ac

thunar/thunar-location-buttons.c
@@ -1267,6 +1267,9 @@ thunar_location_buttons_gone (ThunarLocationButton  *button,
 
   /* drop the button itself */
   gtk_widget_destroy (GTK_WIDGET (button));
+
+  /* clear last_visible_button scroll position */
+  buttons->last_visible_button = NULL;
 }
 
 