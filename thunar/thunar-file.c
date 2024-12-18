xfce-mirror/thunar

commit 021975aad4196f6845331a4463e402c8d9290b22

Add emblem and type description to mount points (#1388)
Fixes #1388
MR xfce/thunar!559

thunar/thunar-file.c
@@ -2523,25 +2523,35 @@ thunar_file_get_content_type_desc (ThunarFile *file)
   const gchar *content_type;
   gchar       *description;
   gchar       *type_text;
-  gchar       *link_text;
 
-  /* thunar_file_get_content_type always provides fallback, hence no NULL check needed */
+  /* determine the content type of the file */
   content_type = thunar_file_get_content_type (file);
-
-  if (G_LIKELY (!thunar_file_is_symlink (file)))
-    return g_content_type_get_description (content_type);
+  if (G_UNLIKELY (content_type == NULL))
+    return g_strdup ("");
 
   /* handle broken symlink */
   if (G_UNLIKELY (g_content_type_equals (content_type, "inode/symlink")))
-    return g_strdup ("broken link");
+    return g_strdup (_("broken link"));
 
   /* append " (link to <target>)" to description if link is not broken */
-  type_text = g_content_type_get_description (content_type);
-  link_text = g_strdup_printf (_("link to %s"), thunar_file_get_symlink_target (file));
-  description = g_strdup_printf ("%s (%s)", type_text, link_text);
-  g_free (link_text);
-  g_free (type_text);
-  return description;
+  if (G_UNLIKELY (thunar_file_is_symlink (file)))
+    {
+      type_text = g_content_type_get_description (content_type);
+      description = g_strdup_printf (_("%s (link to %s)"), type_text, thunar_file_get_symlink_target (file));
+      g_free (type_text);
+      return description;
+    }
+
+  /* append " (mount point)" to description if folder is a mount point */
+  if (G_UNLIKELY (thunar_file_is_mountpoint (file)))
+    {
+      type_text = g_content_type_get_description (content_type);
+      description = g_strdup_printf (_("%s (mount point)"), type_text);
+      g_free (type_text);
+      return description;
+    }
+
+  return g_content_type_get_description (content_type);
 }
 
 
@@ -2890,6 +2900,27 @@ thunar_file_is_mountable (const ThunarFile *file)
 {
   _thunar_return_val_if_fail (THUNAR_IS_FILE (file), FALSE);
   return file->kind == G_FILE_TYPE_MOUNTABLE;
+}
+
+
+
+/**
+ * thunar_file_is_mountpoint:
+ * @file : a #ThunarFile.
+ *
+ * Returns %TRUE if @file is a mount point.
+ *
+ * Return value: %TRUE if @file is a mount point.
+ **/
+gboolean
+thunar_file_is_mountpoint (const ThunarFile *file)
+{
+  _thunar_return_val_if_fail (THUNAR_IS_FILE (file), FALSE);
+
+  if (file->info == NULL)
+    return FALSE;
+
+  return g_file_info_get_attribute_boolean (file->info, G_FILE_ATTRIBUTE_UNIX_IS_MOUNTPOINT);
 }
 
 
@@ -3743,6 +3774,22 @@ thunar_file_get_emblem_names (ThunarFile *file)
        * users won't be surprised when opening the file in a text editor, but are unable to save.
        */
       emblems = g_list_prepend (emblems, g_strdup (THUNAR_FILE_EMBLEM_NAME_CANT_WRITE));
+    }
+
+  /* add mount icon as emblem to mount points */
+  if (thunar_file_is_mountpoint (file))
+    {
+      GMount *mount = g_file_find_enclosing_mount (file->gfile, NULL, NULL);
+      if (mount != NULL)
+        {
+          GIcon       *icon = g_mount_get_icon (mount);
+          const gchar *icon_name = g_themed_icon_get_names (G_THEMED_ICON (icon))[0];
+
+          if (icon_name != NULL)
+            emblems = g_list_prepend (emblems, g_strdup (icon_name));
+
+          g_object_unref (icon);
+        }
     }
 
   return emblems;

thunar/thunar-file.h
@@ -264,6 +264,8 @@ thunar_file_is_shortcut (const ThunarFile *file) G_GNUC_PURE;
 gboolean
 thunar_file_is_mountable (const ThunarFile *file) G_GNUC_PURE;
 gboolean
+thunar_file_is_mountpoint (const ThunarFile *file);
+gboolean
 thunar_file_is_local (const ThunarFile *file);
 gboolean
 thunar_file_is_parent (const ThunarFile *file,

thunar/thunar-util.c
@@ -1455,10 +1455,12 @@ thunar_util_get_statusbar_text_for_single_file (ThunarFile *file)
         temp_string = g_strdup (_ ("shortcut"));
       else if (G_UNLIKELY (thunar_file_get_kind (file) == G_FILE_TYPE_MOUNTABLE))
         temp_string = g_strdup (_ ("mountable"));
-      else
+      else if (G_UNLIKELY (thunar_file_is_mountpoint (file)))
+        temp_string = g_strdup (_ ("mount point"));
+      else if (G_LIKELY (content_type != NULL))
         {
           gchar *description = g_content_type_get_description (content_type);
-          temp_string = g_strdup_printf (_ ("%s"), description);
+          temp_string = g_strdup (description);
           g_free (description);
         }
       text_list = g_list_append (text_list, temp_string);

thunarx/thunarx-file-info.h
@@ -48,6 +48,7 @@ G_BEGIN_DECLS
   "trash::*," \
   "recent::*," \
   "unix::gid,unix::uid,unix::mode," \
+  "unix::is-mountpoint," \
   "metadata::emblems," \
   "metadata::thunar-view-type," \
   "metadata::thunar-sort-column,metadata::thunar-sort-order," \

commit 683117963eaf253e9bf353077965d619fa33c2b2

Prevent crash when loading mount point emblem (#1511)
Fixes: 021975aa

thunar/thunar-file.c
@@ -3782,13 +3782,19 @@ thunar_file_get_emblem_names (ThunarFile *file)
       GMount *mount = g_file_find_enclosing_mount (file->gfile, NULL, NULL);
       if (mount != NULL)
         {
-          GIcon       *icon = g_mount_get_icon (mount);
-          const gchar *icon_name = g_themed_icon_get_names (G_THEMED_ICON (icon))[0];
+          GIcon *icon = g_mount_get_icon (mount);
+          if (icon != NULL)
+            {
+              if (G_IS_THEMED_ICON (icon))
+                {
+                  const gchar *icon_name = g_themed_icon_get_names (G_THEMED_ICON (icon))[0];
 
-          if (icon_name != NULL)
-            emblems = g_list_prepend (emblems, g_strdup (icon_name));
+                  if (icon_name != NULL)
+                    emblems = g_list_prepend (emblems, g_strdup (icon_name));
+                }
 
-          g_object_unref (icon);
+              g_object_unref (icon);
+            }
         }
     }
 