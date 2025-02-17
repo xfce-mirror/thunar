7b0fd42475c31c0a9cccb9ff5db81e9d778ad1bc
Alexander Schwinn <alexxcons@xfce.org>
2025-02-17 18:39:52 +0100

    remove second malloc_trim

--- thunar/thunar-application.c
+++ thunar/thunar-application.c
@@ -2946,11 +2946,4 @@ thunar_application_malloc_trim_on_idle (ThunarApplication *application)
     {
       application->malloc_trim_idle_id = g_idle_add (thunar_application_malloc_trim_idle, application);
     }
-
-#ifdef __GLIBC__
-  /* Workaround to make the kernel reclaim memory which is not used by us anymore. */
-  /* When using glibc, after releasing bigger memory chunks it seems to be required to call that 'garbage collector'. */
-  /* More info here: https://gitlab.xfce.org/xfce/thunar/-/issues/1552#note_101990 for more information */
-  malloc_trim (0);
-#endif
 }
