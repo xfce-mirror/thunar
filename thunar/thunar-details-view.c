xfce-mirror/thunar

commit c1682a091de5103e0912392231fed0ac9effb8e2

Dont change selection on activate
Reverts 99ba79ed, which is not needed any more and causes trouble now
See !508

thunar/thunar-details-view.c
@@ -152,9 +152,6 @@ struct _ThunarDetailsView
   /* whether to use fixed column widths */
   gboolean           fixed_columns;
 
-  /* whether the most recent item activation used a mouse button press */
-  gboolean           button_pressed;
-
   /* event source id for thunar_details_view_zoom_level_changed_reload_fixed_height */
   guint idle_id;
 
@@ -807,8 +804,6 @@ thunar_details_view_button_press_event (GtkTreeView       *tree_view,
     {
       GtkTreePath       *cursor_path;
 
-      details_view->button_pressed = TRUE;
-
       /* find out if the expander was clicked;
        * only needed if expandable folders is enabled */
       if (gtk_tree_view_get_show_expanders (tree_view))
@@ -1105,8 +1100,6 @@ thunar_details_view_key_press_event (GtkTreeView       *tree_view,
   GtkTreeIter       iter;
   ThunarFile       *file = NULL;
 
-  details_view->button_pressed = FALSE;
-
   /* popup context menu if "Menu" or "<Shift>F10" is pressed */
   if (event->keyval == GDK_KEY_Menu || ((event->state & GDK_SHIFT_MASK) != 0 && event->keyval == GDK_KEY_F10))
     {
@@ -1201,19 +1194,10 @@ thunar_details_view_row_activated (GtkTreeView       *tree_view,
                                    GtkTreeViewColumn *column,
                                    ThunarDetailsView *details_view)
 {
-  GtkTreeSelection    *selection;
   ThunarActionManager *action_mgr;
   GtkWidget           *window;
 
   _thunar_return_if_fail (THUNAR_IS_DETAILS_VIEW (details_view));
-
-  /* be sure to have only the clicked item selected */
-  if (details_view->button_pressed)
-    {
-      selection = gtk_tree_view_get_selection (tree_view);
-      gtk_tree_selection_unselect_all (selection);
-      gtk_tree_selection_select_path (selection, path);
-    }
 
   window = gtk_widget_get_toplevel (GTK_WIDGET (details_view));
   action_mgr = thunar_window_get_action_manager (THUNAR_WINDOW (window));

thunar/thunar-abstract-icon-view.c
@@ -96,8 +96,6 @@ struct _ThunarAbstractIconViewPrivate
   gulong gesture_expose_id;
   gulong gesture_motion_id;
   gulong gesture_release_id;
-
-  gboolean button_pressed;
 };
 
 
@@ -399,8 +397,6 @@ thunar_abstract_icon_view_button_press_event (ExoIconView            *view,
   GtkTreePath       *path;
   GtkWidget         *window;
 
-  abstract_icon_view->priv->button_pressed = TRUE;
-
   /* give focus to the clicked view */
   window = gtk_widget_get_toplevel (GTK_WIDGET (abstract_icon_view));
   thunar_window_focus_view (THUNAR_WINDOW (window), GTK_WIDGET (abstract_icon_view));
@@ -585,8 +581,6 @@ thunar_abstract_icon_view_key_press_event (ExoIconView            *view,
                                            GdkEventKey            *event,
                                            ThunarAbstractIconView *abstract_icon_view)
 {
-  abstract_icon_view->priv->button_pressed = FALSE;
-
   /* popup context menu if "Menu" or "<Shift>F10" is pressed */
   if (event->keyval == GDK_KEY_Menu || ((event->state & GDK_SHIFT_MASK) != 0 && event->keyval == GDK_KEY_F10))
     {
@@ -645,13 +639,6 @@ thunar_abstract_icon_view_item_activated (ExoIconView            *view,
   GtkWidget *window;
 
   _thunar_return_if_fail (THUNAR_IS_ABSTRACT_ICON_VIEW (abstract_icon_view));
-
-  /* be sure to have only the clicked item selected */
-  if (abstract_icon_view->priv->button_pressed)
-    {
-      exo_icon_view_unselect_all (view);
-      exo_icon_view_select_path (view, path);
-    }
 
   window = gtk_widget_get_toplevel (GTK_WIDGET (abstract_icon_view));
   thunar_action_manager_activate_selected_files (thunar_window_get_action_manager (THUNAR_WINDOW (window)), THUNAR_ACTION_MANAGER_CHANGE_DIRECTORY, NULL);

commit 3dd992934b014704f6ff2a1fad6d5f6b8d494640

Fix item activation on double-click in list view (#1567)
Clicks on columns other than the "Name" column will unselect all items
to allow rubber banding. Therefore, manually re-select the current item
on double-click events to allow item activation.

Fixes: c1682a09

thunar/thunar-details-view.c
@@ -1238,6 +1238,9 @@ thunar_details_view_row_activated (GtkTreeView       *tree_view,
 
   _thunar_return_if_fail (THUNAR_IS_DETAILS_VIEW (details_view));
 
+  /* ensure that the path is selected on double-click events */
+  thunar_details_view_select_path (THUNAR_STANDARD_VIEW (details_view), path);
+
   window = gtk_widget_get_toplevel (GTK_WIDGET (details_view));
   action_mgr = thunar_window_get_action_manager (THUNAR_WINDOW (window));
   thunar_action_manager_activate_selected_files (action_mgr, THUNAR_ACTION_MANAGER_CHANGE_DIRECTORY, NULL);