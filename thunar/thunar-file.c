47fcca22bd10dd4bc01faf9ecda6bbe617792929
Stealthninja <realstealthninja@gmail.com>
2025-02-23 16:53:39 +0530

    change variable name from should_thumbnail to request_thumbnail

--- thunar/thunar-file.c
+++ thunar/thunar-file.c
@@ -5408,16 +5408,16 @@ thunar_file_request_thumbnail (ThunarFile         *file,
 
   if (g_strv_length (mime_types_whitelist) != 0)
     {
-      gboolean should_thumbail = FALSE;
+      gboolean request_thumbnail = FALSE;
       for (size_t i = 0; i < g_strv_length (mime_types_whitelist); i++)
         {
           if (strncmp (thunar_file_get_content_type (file), mime_types_whitelist[i], g_utf8_strlen (mime_types_whitelist[i], 53)) == 0)
             {
-              should_thumbail = TRUE;
+              request_thumbnail = TRUE;
             }
         }
 
-      if (should_thumbail == FALSE)
+      if (request_thumbnail == FALSE)
         {
           file->thumbnail_state[size] = THUNAR_FILE_THUMB_STATE_NONE;
         }
