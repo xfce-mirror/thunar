9d2cd392b199b4d5d3a3c2c339585e9f81497ec7
joyal <realstealthninja@gmail.com>
2025-03-01 04:28:31 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Alexander Schwinn <alexxcons@xfce.org>

--- thunar/thunar-file.c
+++ thunar/thunar-file.c
@@ -5397,9 +5397,7 @@ thunar_file_request_thumbnail (ThunarFile            *file,
   _thunar_return_if_fail (THUNAR_IS_FILE (file));
 
   if (file->thumbnail_state[size] == THUNAR_FILE_THUMB_STATE_NONE && purpose != THUNAR_THUMBNAIL_PURPOSE_DEFAULT)
-    {
-      file->thumbnail_state[size] = THUNAR_FILE_THUMB_STATE_UNKNOWN;
-    }
+    file->thumbnail_state[size] = THUNAR_FILE_THUMB_STATE_UNKNOWN;
 
   /* For all other states, the thumbnailer already processed the file or is currently working on it */
   if (file->thumbnail_state[size] != THUNAR_FILE_THUMB_STATE_UNKNOWN)
