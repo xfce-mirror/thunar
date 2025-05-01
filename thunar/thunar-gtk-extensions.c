xfce-mirror/thunar

commit 63aa3a9a53d4d1ab67274f0c2b811669d0fa9716

wayland: Fix popup menu not closing (#1592)
Implement our own version of gtk_menu_popup_at_pointer() to work around
a GTK3 Wayland bug that prevents the menu from being dismissed via click
in certain areas outside of the menu.
The main difference is the added gdk_window_get_toplevel() call.

See: https://gitlab.gnome.org/GNOME/gtk/-/issues/160

thunar/thunar-gtk-extensions.c
@@ -186,7 +186,7 @@ thunar_gtk_menu_clean (GtkMenu *menu)
  * thunar_gtk_menu_run:
  * @menu : a #GtkMenu.
  *
- * Conveniance wrapper for thunar_gtk_menu_run_at_event_pointer, to run a menu for the current event
+ * Convenience wrapper for thunar_gtk_menu_run_at_event() to run a menu for the current event
  **/
 void
 thunar_gtk_menu_run (GtkMenu *menu)
@@ -206,6 +206,51 @@ thunar_gtk_menu_run (GtkMenu *menu)
 
 
 
+static void
+thunar_gtk_menu_popup_at_pointer (GtkMenu  *menu,
+                                  GdkEvent *event)
+{
+  GdkWindow   *window;
+  GdkWindow   *toplevel = NULL;
+  GdkDevice   *device;
+  GdkRectangle rect = { 0, 0, 1, 1 };
+
+  /* fallback if event not set */
+  if (event == NULL)
+    {
+      gtk_menu_popup_at_pointer (menu, event);
+      return;
+    }
+
+  /* create popup rect */
+  window = gdk_event_get_window (event);
+  if (window != NULL)
+    {
+      /* Ensure that we have the toplevel window to work around a GTK3 Wayland bug
+       * that prevents the user from dismissing the menu via click in some cases.
+       * See: https://gitlab.xfce.org/xfce/thunar/-/issues/1592 */
+      toplevel = gdk_window_get_toplevel (window);
+
+      device = gdk_event_get_device (event);
+
+      if (device != NULL && gdk_device_get_source (device) == GDK_SOURCE_KEYBOARD)
+        device = gdk_device_get_associated_device (device);
+
+      if (device != NULL)
+        gdk_window_get_device_position (toplevel, device, &rect.x, &rect.y, NULL);
+    }
+
+  /* pop up the menu at the rectangle below the mouse cursor */
+  gtk_menu_popup_at_rect (menu,
+                          toplevel,
+                          &rect,
+                          GDK_GRAVITY_NORTH_WEST,
+                          GDK_GRAVITY_NORTH_WEST,
+                          event);
+}
+
+
+
 /**
  * thunar_gtk_menu_run_at_event:
  * @menu  : a #GtkMenu.
@@ -220,7 +265,8 @@ thunar_gtk_menu_run (GtkMenu *menu)
  *
  **/
 void
-thunar_gtk_menu_run_at_event (GtkMenu *menu, GdkEvent *event)
+thunar_gtk_menu_run_at_event (GtkMenu  *menu,
+                              GdkEvent *event)
 {
   GMainLoop *loop;
   gulong     signal_id;
@@ -233,7 +279,7 @@ thunar_gtk_menu_run_at_event (GtkMenu *menu, GdkEvent *event)
   /* run an internal main loop */
   loop = g_main_loop_new (NULL, FALSE);
   signal_id = g_signal_connect_swapped (G_OBJECT (menu), "deactivate", G_CALLBACK (g_main_loop_quit), loop);
-  gtk_menu_popup_at_pointer (menu, event);
+  thunar_gtk_menu_popup_at_pointer (menu, event);
   gtk_menu_reposition (menu);
   gtk_grab_add (GTK_WIDGET (menu));
   g_main_loop_run (loop);

commit 1e2f8cf5c6d1a234a08c47ea4d84444edb6875b4

Fix popup position of DnD menu
The menu is misplaced when doing a right-click drag-and-drop from
xfdesktop to Thunar ("Event with type 19 not holding a GdkSeat").
So, skip using the toplevel window workaround in this case.

Fixes: 63aa3a9a

thunar/thunar-gtk-extensions.c
@@ -215,8 +215,8 @@ thunar_gtk_menu_popup_at_pointer (GtkMenu  *menu,
   GdkDevice   *device;
   GdkRectangle rect = { 0, 0, 1, 1 };
 
-  /* fallback if event not set */
-  if (event == NULL)
+  /* fallback if event not set or event not holding a GdkSeat */
+  if (event == NULL || gdk_event_get_seat (event) == NULL)
     {
       gtk_menu_popup_at_pointer (menu, event);
       return;