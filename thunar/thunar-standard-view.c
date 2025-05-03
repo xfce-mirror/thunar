xfce-mirror/thunar

commit 4cab2a085a4292b19d8cf655b645d2a188a542e1

Allow DnD from host to guest VM (#113)
We simply add the missing "text/uri-list" type to the target check in
thunar_standard_view_drag_motion. The main view can now act as a target
for drag-and-drop actions from the host to a VirtualBox guest instance.

thunar/thunar-standard-view.c
@@ -3651,7 +3651,10 @@ thunar_standard_view_drag_motion (GtkWidget          *view,
       /* check if we can handle that drag data (yet?) */
       target = gtk_drag_dest_find_target (view, context, NULL);
 
-      if (target == gdk_atom_intern_static_string ("XdndDirectSave0") || target == gdk_atom_intern_static_string ("_NETSCAPE_URL") || target == gdk_atom_intern_static_string ("application/octet-stream"))
+      if (target == gdk_atom_intern_static_string ("XdndDirectSave0")
+          || target == gdk_atom_intern_static_string ("_NETSCAPE_URL")
+          || target == gdk_atom_intern_static_string ("application/octet-stream")
+          || target == gdk_atom_intern_static_string ("text/uri-list"))
         {
           /* determine the file for the given coordinates */
           file = thunar_standard_view_get_drop_file (standard_view, x, y, &path);

commit a02142796d1f8fcc8948be469d956e97095f72dc

Fix VirtualBox DnD detection (#113)
We have to use a different check to detect and handle a drag-and-drop
from the host system to Thunar running in a VirtualBox instance.

See: !642
Fixes: 4cab2a08

thunar/thunar-standard-view.c
@@ -3644,17 +3644,19 @@ thunar_standard_view_drag_motion (GtkWidget          *view,
   GtkTreePath  *path;
   ThunarFile   *file = NULL;
   GdkAtom       target;
+  GdkWindow    *source_window;
 
   /* request the drop data on-demand (if we don't have it already) */
   if (G_UNLIKELY (!standard_view->priv->drop_data_ready))
     {
       /* check if we can handle that drag data (yet?) */
       target = gtk_drag_dest_find_target (view, context, NULL);
+      source_window = gdk_drag_context_get_source_window (context);
 
       if (target == gdk_atom_intern_static_string ("XdndDirectSave0")
           || target == gdk_atom_intern_static_string ("_NETSCAPE_URL")
           || target == gdk_atom_intern_static_string ("application/octet-stream")
-          || target == gdk_atom_intern_static_string ("text/uri-list"))
+          || (target != GDK_NONE && source_window != NULL && !gdk_window_is_visible (source_window))) /* VBox DnD detection */
         {
           /* determine the file for the given coordinates */
           file = thunar_standard_view_get_drop_file (standard_view, x, y, &path);