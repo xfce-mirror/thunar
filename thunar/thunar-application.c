6965a86cecd6389008396933c8b245dc9fe3a130
Shawn Henson <shawn@shenso.name>
2025-05-05 17:39:52 -0400

    Always warn user before permanent deleting files
    The warn argument on thunar_application_unlink_files currently applies
    to both trashing and permanent deletes. Upstream trash functions do not
    account for the permanent argument being mutable, so permanent deletes
    occur without warning when any file in the file list cannot be trashed.
    
    This commit changes thunar_application_unlink_files to only reference
    the warn argument when moving files to trash, and assume all permanent
    deletions should ask the user for confirmation.
    
    Fixes: #1621

--- thunar/thunar-application.c
+++ thunar/thunar-application.c
@@ -2444,7 +2444,7 @@ unlink_stub (GList *source_path_list,
  * @parent      : a #GdkScreen, a #GtkWidget or %NULL.
  * @file_list   : the list of #ThunarFile<!---->s that should be deleted.
  * @permanently : whether to unlink the files permanently.
- * @warn        : whether to warn the user if deleting permanently.
+ * @warn        : whether to warn the user if moving files to trash.
  * @log_mode    : log mode
  *
  * Deletes all files in the @file_list and takes care of all user interaction.
@@ -2495,7 +2495,7 @@ thunar_application_unlink_files (ThunarApplication           *application,
     return FALSE;
 
   /* ask the user to confirm if deleting permanently */
-  if (G_UNLIKELY (permanently) && warn)
+  if (G_UNLIKELY (permanently))
     {
       /* parse the parent pointer */
       screen = thunar_util_parse_parent (parent, &window);
@@ -2560,12 +2560,6 @@ thunar_application_unlink_files (ThunarApplication           *application,
         }
       else
         operation_canceled = TRUE;
-    }
-  else if (G_UNLIKELY (permanently))
-    {
-      thunar_application_launch (application, parent, "edit-delete",
-                                 _("Deleting files..."), unlink_stub,
-                                 path_list, path_list, TRUE, FALSE, log_mode, NULL);
     }
   else if (G_LIKELY (!permanently) && warn)
     {
