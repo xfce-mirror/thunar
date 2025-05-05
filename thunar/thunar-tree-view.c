0693c5754144b8278bb28ce5abbe683fbe81ec5c
Shawn Henson <shawn@shenso.name>
2025-05-05 19:26:59 -0400

    Refactor thunar_application_unlink_files
    Remove warn argument and check the `misc-confirm-move-to-trash`
    preference before trashing instead of doing so repeatedly upstream.

--- thunar/thunar-tree-view.c
+++ thunar/thunar-tree-view.c
@@ -1498,7 +1498,6 @@ thunar_tree_view_action_unlink_selected_folder (ThunarTreeView *view,
   ThunarApplication *application;
   ThunarFile        *file;
   GList              file_list;
-  gboolean           warn;
 
   _thunar_return_if_fail (THUNAR_IS_TREE_VIEW (view));
 
@@ -1513,13 +1512,8 @@ thunar_tree_view_action_unlink_selected_folder (ThunarTreeView *view,
           file_list.next = NULL;
           file_list.prev = NULL;
 
-          /* delete the file */
-          if (permanently)
-            warn = TRUE;
-          else
-            g_object_get (G_OBJECT (view->preferences), "misc-confirm-move-to-trash", &warn, NULL);
           application = thunar_application_get ();
-          thunar_application_unlink_files (application, GTK_WIDGET (view), &file_list, permanently, warn, THUNAR_OPERATION_LOG_OPERATIONS);
+          thunar_application_unlink_files (application, GTK_WIDGET (view), &file_list, permanently, THUNAR_OPERATION_LOG_OPERATIONS);
           g_object_unref (G_OBJECT (application));
         }
 
