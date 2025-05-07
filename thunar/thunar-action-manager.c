c7d584e2e54497433e44df6077879150e5ae65d8
Shawn Henson <shawn@shenso.name>
2025-05-07 00:23:43 -0400

    Decouple trash and unlinking interfaces
    The method `thunar_application_unlink_files` now only creates an unlink
    IO job, and does not handle any trash logic. The method
    `thunar_application_trash` has been renamed
    `thunar_application_trash_files` making both of the aformentioned method
    names symmetrical to the names of their IO jobs. Both methods now handle
    all user interaction for these operations.

--- thunar/thunar-action-manager.c
+++ thunar/thunar-action-manager.c
@@ -2637,7 +2637,7 @@ thunar_action_manager_action_move_to_trash (ThunarActionManager *action_mgr)
     return;
 
   application = thunar_application_get ();
-  thunar_application_unlink_files (application, action_mgr->widget, action_mgr->files_to_process, FALSE, THUNAR_OPERATION_LOG_OPERATIONS);
+  thunar_application_trash_files (application, action_mgr->widget, action_mgr->files_to_process, THUNAR_OPERATION_LOG_OPERATIONS);
   g_object_unref (G_OBJECT (application));
 }
 
@@ -2654,7 +2654,7 @@ thunar_action_manager_action_delete (ThunarActionManager *action_mgr)
     return TRUE;
 
   application = thunar_application_get ();
-  thunar_application_unlink_files (application, action_mgr->widget, action_mgr->files_to_process, TRUE, THUNAR_OPERATION_LOG_OPERATIONS);
+  thunar_application_unlink_files (application, action_mgr->widget, action_mgr->files_to_process, THUNAR_OPERATION_LOG_OPERATIONS);
   g_object_unref (G_OBJECT (application));
 
   /* required in case of shortcut activation, in order to signal that the accel key got handled */
