f69b88bf59cb744721a8b54904c466415de794de
Zetta1Reid0 <79805086+Zetta1Reid0@users.noreply.github.com>
2025-05-11 09:41:54 +0300

    Thunar transfer job, count nodes in subfolders
    Revert "fIle operation dialog: always show "apply to all" box"
    
    This reverts commit 893eba1019b29af6003197bb7ff8388b95e46c52.
    
    Thunar job - count items in subfolders
    
    clang fix
    
    Apply 1 suggestion(s) to 1 file(s)
    
    Co-authored-by: Alexander Schwinn <alexxcons@xfce.org>
    
    Update file thunar-transfer-job.c

--- thunar/thunar-transfer-job.c
+++ thunar/thunar-transfer-job.c
@@ -391,6 +391,7 @@ thunar_transfer_job_collect_node (ThunarTransferJob  *job,
                                   ThunarTransferNode *node,
                                   GError            **error)
 {
+  guint               n_total_files;
   ThunarTransferNode *child_node;
   GFileInfo          *info;
   GError             *err = NULL;
@@ -422,6 +423,12 @@ thunar_transfer_job_collect_node (ThunarTransferJob  *job,
       file_list = thunar_io_scan_directory (THUNAR_JOB (job), node->source_file,
                                             G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
                                             FALSE, FALSE, FALSE, NULL, &err);
+
+      /* append Children to number of total files */
+      n_total_files = thunar_job_get_n_total_files (THUNAR_JOB (job));
+      n_total_files = thunar_job_get_n_total_files (THUNAR_JOB (job)) + g_list_length (file_list);
+
+      thunar_job_set_n_total_files (THUNAR_JOB (job), n_total_files);
 
       /* add children to the transfer node */
       for (lp = file_list; err == NULL && lp != NULL; lp = lp->next)
