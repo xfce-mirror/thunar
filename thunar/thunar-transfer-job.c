83b854157a1b9afd25c39a370e1f6cd6292c117f
Zetta1  Reid0 <11255-Zetta1_Reid0@users.noreply.gitlab.xfce.org>
2025-05-17 08:13:46 +0000

    Update file thunar-transfer-job.c

--- thunar/thunar-transfer-job.c
+++ thunar/thunar-transfer-job.c
@@ -391,7 +391,6 @@ thunar_transfer_job_collect_node (ThunarTransferJob  *job,
                                   ThunarTransferNode *node,
                                   GError            **error)
 {
-  ThunarJob          *thunar_job;
   guint               n_total_files;
   ThunarTransferNode *child_node;
   GFileInfo          *info;
@@ -426,12 +425,10 @@ thunar_transfer_job_collect_node (ThunarTransferJob  *job,
                                             FALSE, FALSE, FALSE, NULL, &err);
 
       /* append Children to number of total files */
-      thunar_job = THUNAR_JOB (job);
-
-      n_total_files = thunar_job_get_n_total_files (thunar_job);
+      n_total_files = thunar_job_get_n_total_files (THUNAR_JOB (job));
       n_total_files = thunar_job_get_n_total_files (THUNAR_JOB (job)) + g_list_length (file_list);
 
-      thunar_job_set_n_total_files (thunar_job, n_total_files);
+      thunar_job_set_n_total_files (THUNAR_JOB (job), n_total_files);
 
       /* add children to the transfer node */
       for (lp = file_list; err == NULL && lp != NULL; lp = lp->next)
