f0eaf6fc5616913d8b77c6e3d73f4283c550e2c1
Alexander Schwinn <alexxcons@xfce.org>
2025-06-30 23:00:11 +0200

    Improve file add/remove checks for ThunarFolder (#1649)
    - Do not allow idle-add ThunarFiles to ThunarFolder if they are already present
    - Do not allow to idle-remove ThunarFiles of a ThunarFolder if the folder does not have the file.

--- thunar/thunar-folder.c
+++ thunar/thunar-folder.c
@@ -643,8 +643,13 @@ static void
 thunar_folder_add_file (ThunarFolder *folder,
                         ThunarFile   *file)
 {
+
   /* If it possibly was removed shortly before, just undo the "remove" */
   if (g_hash_table_remove (folder->removed_files_map, file))
+    return;
+
+  /* Do nothing if we already have the file in our internal map */
+  if (g_hash_table_lookup (folder->files_map, file) != NULL)
     return;
 
   g_hash_table_add (folder->added_files_map, g_object_ref (file));
@@ -661,6 +666,10 @@ thunar_folder_remove_file (ThunarFolder *folder,
 {
   /* If it possibly was added shortly before, just undo the "add" */
   if (g_hash_table_remove (folder->added_files_map, file))
+    return;
+
+  /* Do nothing if we dont have the file yet in our internal map */
+  if (g_hash_table_lookup (folder->files_map, file) == NULL)
     return;
 
   g_hash_table_add (folder->removed_files_map, g_object_ref (file));
