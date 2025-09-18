xfce-mirror/thunar

commit e653de0103a954dc68db8624a314bb9269577f62

Always re-add files to cache on reload (#1357)
This is required in order to still locate such ThunarFiles, after a G_FILE_MONITOR_EVENT_DELETED is received.
Otherwise it can happen that fail to react to iG_FILE_MONITOR_EVENT_DELETED, even if the related ThunarFile is still used in views.

thunar/thunar-file.c
@@ -1181,7 +1181,8 @@ thunar_file_load (ThunarFile   *file,
                   GCancellable *cancellable,
                   GError      **error)
 {
-  GError *err = NULL;
+  GError    *err = NULL;
+  GFileInfo *info = NULL;
 
   _thunar_return_val_if_fail (THUNAR_IS_FILE (file), FALSE);
   _thunar_return_val_if_fail (error == NULL || *error == NULL, FALSE);
@@ -1193,14 +1194,11 @@ thunar_file_load (ThunarFile   *file,
   /* remove the file from cache */
   g_hash_table_remove (file_cache, file->gfile);
 
-  /* reset the file */
-  thunar_file_info_clear (file);
-
   /* query a new file info */
-  file->info = g_file_query_info (file->gfile,
-                                  THUNARX_FILE_INFO_NAMESPACE,
-                                  G_FILE_QUERY_INFO_NONE,
-                                  cancellable, &err);
+  info = g_file_query_info (file->gfile,
+                            THUNARX_FILE_INFO_NAMESPACE,
+                            G_FILE_QUERY_INFO_NONE,
+                            cancellable, &err);
 
   /* update the mounted info */
   if (err != NULL
@@ -1213,10 +1211,24 @@ thunar_file_load (ThunarFile   *file,
 
   if (err != NULL)
     {
+      if (info != NULL)
+        g_object_unref (info);
+
       g_propagate_error (error, err);
+
+      /* even if we failed to load it, re-add the file into the cache */
+      g_hash_table_insert (file_cache,
+                           g_object_ref (file->gfile),
+                           weak_ref_new (G_OBJECT (file)));
       G_REC_UNLOCK (file_cache_mutex);
       return FALSE;
     }
+
+  /* reset the file */
+  thunar_file_info_clear (file);
+
+  /* update the file with the new info */
+  file->info = info;
 
   /* update the file from the information */
   thunar_file_info_reload (file, cancellable);

commit 265a5a1855499838f74470bbf26a592d275b93ab

Fix 'MOUNTED' state on file reload when unmounted
Regression was introduced in e653de01

MR xfce/thunar!703

thunar/thunar-file.c
@@ -1230,6 +1230,7 @@ thunar_file_load (ThunarFile   *file,
 {
   GError    *err = NULL;
   GFileInfo *info = NULL;
+  gboolean   mounted = TRUE;
 
   _thunar_return_val_if_fail (THUNAR_IS_FILE (file), FALSE);
   _thunar_return_val_if_fail (error == NULL || *error == NULL, FALSE);
@@ -1247,12 +1248,12 @@ thunar_file_load (ThunarFile   *file,
                             G_FILE_QUERY_INFO_NONE,
                             cancellable, &err);
 
-  /* update the mounted info */
+  /* check if mounted (but do not update the file until after it has been cleared) */
   if (err != NULL
       && err->domain == G_IO_ERROR
       && err->code == G_IO_ERROR_NOT_MOUNTED)
     {
-      FLAG_UNSET (file, THUNAR_FILE_FLAG_IS_MOUNTED);
+      mounted = FALSE;
       g_clear_error (&err);
     }
 
@@ -1279,6 +1280,10 @@ thunar_file_load (ThunarFile   *file,
 
   /* update the file from the information */
   thunar_file_info_reload (file, cancellable);
+
+  /* update the mounted info */
+  if (mounted == FALSE)
+    FLAG_UNSET (file, THUNAR_FILE_FLAG_IS_MOUNTED);
 
   /* (re)insert the file into the cache */
   if (file->kind != G_FILE_TYPE_UNKNOWN)