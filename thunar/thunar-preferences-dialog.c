1203dbe06eaa712e52dec96b6e556e22a30caf5b
Dmitry Petrachkov <dmitry-petrachkov@outlook.com>
2025-09-24 20:30:18 +0300

    improve-disk-space-usage-bar: Improvements

--- thunar/thunar-preferences-dialog.c
+++ thunar/thunar-preferences-dialog.c
@@ -678,7 +678,19 @@ thunar_preferences_dialog_init (ThunarPreferencesDialog *dialog)
   gtk_widget_show (button);
 
   /* next row */
-  row++;
+  frame = g_object_new (GTK_TYPE_FRAME, "border-width", 0, "shadow-type", GTK_SHADOW_NONE, NULL);
+  gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, TRUE, 0);
+  gtk_widget_show (frame);
+
+  grid = gtk_grid_new ();
+  gtk_grid_set_column_spacing (GTK_GRID (grid), 12);
+  gtk_grid_set_row_spacing (GTK_GRID (grid), 6);
+  gtk_widget_set_margin_top (GTK_WIDGET (grid), 6);
+  gtk_widget_set_margin_start (GTK_WIDGET (grid), 12);
+  gtk_container_add (GTK_CONTAINER (frame), grid);
+  gtk_widget_show (grid);
+
+  row = 0;
 
   button = gtk_check_button_new_with_mnemonic (_("Show disk space _usage bar"));
   g_object_bind_property (G_OBJECT (dialog->preferences),
@@ -688,6 +700,69 @@ thunar_preferences_dialog_init (ThunarPreferencesDialog *dialog)
                           G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
   gtk_widget_set_hexpand (button, TRUE);
   gtk_grid_attach (GTK_GRID (grid), button, 0, row, 2, 1);
+  gtk_widget_show (button);
+
+  /* next row */
+  row++;
+
+
+  label = gtk_label_new_with_mnemonic (_("Percentage to show 'Storage space low' color"));
+  g_object_bind_property (G_OBJECT (dialog->preferences),
+                          "shortcuts-disk-space-usage-bar",
+                          G_OBJECT (label),
+                          "sensitive",
+                          G_BINDING_SYNC_CREATE);
+  gtk_label_set_xalign (GTK_LABEL (label), 0.0f);
+  gtk_grid_attach (GTK_GRID (grid), label, 0, row, 1, 1);
+  gtk_widget_show (label);
+
+  button = gtk_spin_button_new_with_range (0, 100, 1);
+  g_object_bind_property (G_OBJECT (dialog->preferences),
+                          "shortcuts-disk-space-usage-bar",
+                          G_OBJECT (button),
+                          "sensitive",
+                          G_BINDING_SYNC_CREATE);
+  g_object_bind_property (G_OBJECT (dialog->preferences),
+                          "shortcuts-disk-space-usage-orange-percent",
+                          G_OBJECT (button),
+                          "value",
+                          G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
+  gtk_grid_attach (GTK_GRID (grid), button, 1, row, 1, 1);
+  thunar_gtk_label_set_a11y_relation (GTK_LABEL (label), button);
+  gtk_label_set_mnemonic_widget (GTK_LABEL (label), button);
+  gtk_widget_set_halign (button, GTK_ALIGN_START);
+  gtk_widget_set_hexpand (button, TRUE);
+  gtk_widget_show (button);
+
+  /* next row */
+  row++;
+
+  label = gtk_label_new_with_mnemonic (_("Percentage to show 'Storage almost full' color"));
+  g_object_bind_property (G_OBJECT (dialog->preferences),
+                          "shortcuts-disk-space-usage-bar",
+                          G_OBJECT (label),
+                          "sensitive",
+                          G_BINDING_SYNC_CREATE);
+  gtk_label_set_xalign (GTK_LABEL (label), 0.0f);
+  gtk_grid_attach (GTK_GRID (grid), label, 0, row, 1, 1);
+  gtk_widget_show (label);
+
+  button = gtk_spin_button_new_with_range (0, 100, 1);
+  g_object_bind_property (G_OBJECT (dialog->preferences),
+                          "shortcuts-disk-space-usage-bar",
+                          G_OBJECT (button),
+                          "sensitive",
+                          G_BINDING_SYNC_CREATE);
+  g_object_bind_property (G_OBJECT (dialog->preferences),
+                          "shortcuts-disk-space-usage-red-percent",
+                          G_OBJECT (button),
+                          "value",
+                          G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
+  gtk_grid_attach (GTK_GRID (grid), button, 1, row, 1, 1);
+  thunar_gtk_label_set_a11y_relation (GTK_LABEL (label), button);
+  gtk_label_set_mnemonic_widget (GTK_LABEL (label), button);
+  gtk_widget_set_halign (button, GTK_ALIGN_START);
+  gtk_widget_set_hexpand (button, TRUE);
   gtk_widget_show (button);
 
   /* Tree */
