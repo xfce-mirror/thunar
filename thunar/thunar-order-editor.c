197fc42f8fcd85342d27cd8371ac56515802df0b
Dmitry Petrachkov <dmitry-petrachkov@outlook.com>
2025-09-30 17:32:53 +0300

    order-editor: Copyright

--- thunar/thunar-order-editor.c
+++ thunar/thunar-order-editor.c
@@ -1,3 +1,22 @@
+/* vi:set et ai sw=2 sts=2 ts=2: */
+/*-
+ * Copyright (c) 2025 The Xfce Development Team
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include "thunar/thunar-order-editor.h"
 
 #include <libxfce4ui/libxfce4ui.h>
