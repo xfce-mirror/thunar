67cd23b2977f8a9d75b0d261fabb3708734be4cb
Alexander Schwinn <alexxcons@xfce.org>
2025-10-06 08:21:27 +0200

    Fix rename via tree-view side-pane (#1726)
    Regression introduced in ed4cc260

--- thunar/thunar-dialogs.c
+++ thunar/thunar-dialogs.c
@@ -232,7 +232,7 @@ thunar_dialogs_show_rename_file (gpointer               parent,
   gint               parent_width = 500;
   gint               row = 0;
 
-  _thunar_return_val_if_fail (parent == NULL || GDK_IS_SCREEN (parent) || GTK_IS_WINDOW (parent), FALSE);
+  _thunar_return_val_if_fail (parent == NULL || GDK_IS_SCREEN (parent) || GTK_IS_WIDGET (parent), FALSE);
   _thunar_return_val_if_fail (THUNAR_IS_FILE (file), FALSE);
 
   /* parse the parent window */
