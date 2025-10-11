974ca4b1ee7982ca697e3e70c68aac53b86f37b8
Dmitry Petrachkov <dmitry-petrachkov@outlook.com>
2025-10-11 19:58:28 +0300

    DiskSpaceUsageBar: Change colors when disk is full, adjustable size
    - Different color for "normal" "disk full" and "disk almost full"
    - The limits for which the color is changed is configurable in the thunar preferences
    - The concrete color and the height of the bar can be configured via CSS
    
    MR xfce/thunar!711

--- thunar/thunar-shortcuts-view.c
+++ thunar/thunar-shortcuts-view.c
@@ -343,6 +343,15 @@ thunar_shortcuts_view_init (ThunarShortcutsView *view)
   g_signal_connect_swapped (G_OBJECT (view->preferences), "notify::shortcuts-disk-space-usage-bar",
                             G_CALLBACK (gtk_tree_view_columns_autosize), view);
 
+  g_signal_connect_swapped (G_OBJECT (view->preferences), "notify::shortcuts-disk-space-usage-bar-height",
+                            G_CALLBACK (gtk_tree_view_columns_autosize), view);
+
+  g_signal_connect_swapped (G_OBJECT (view->preferences), "notify::shortcuts-disk-space-usage-warning-percent",
+                            G_CALLBACK (gtk_widget_queue_draw), view);
+
+  g_signal_connect_swapped (G_OBJECT (view->preferences), "notify::shortcuts-disk-space-usage-error-percent",
+                            G_CALLBACK (gtk_widget_queue_draw), view);
+
   /* allocate a single column for our renderers */
   column = view->column = g_object_new (GTK_TYPE_TREE_VIEW_COLUMN,
                                         "reorderable", FALSE,
@@ -412,6 +421,14 @@ thunar_shortcuts_view_init (ThunarShortcutsView *view)
 
   g_object_bind_property (G_OBJECT (view->preferences), "shortcuts-disk-space-usage-bar",
                           G_OBJECT (view->name_renderer), "disk-space-usage-bar-enabled",
+                          G_BINDING_SYNC_CREATE);
+
+  g_object_bind_property (G_OBJECT (view->preferences), "shortcuts-disk-space-usage-warning-percent",
+                          G_OBJECT (view->name_renderer), "disk-space-usage-warning-percent",
+                          G_BINDING_SYNC_CREATE);
+
+  g_object_bind_property (G_OBJECT (view->preferences), "shortcuts-disk-space-usage-error-percent",
+                          G_OBJECT (view->name_renderer), "disk-space-usage-error-percent",
                           G_BINDING_SYNC_CREATE);
 
   /* spinner to indicate (un)mount/eject delay */
