ac86aa34fbbc37c6c10f74c049f1e1805a0b5fae
Alexander Schwinn <alexxcons@xfce.org>
2025-12-18 18:03:00 +0100

    ListView: Ellipsize in middle (#1765)
    Fixes #1765

--- thunar/thunar-details-view.c
+++ thunar/thunar-details-view.c
@@ -404,7 +404,7 @@ thunar_details_view_init (ThunarDetailsView *details_view)
 
           /* add the name renderer */
           g_object_set (G_OBJECT (THUNAR_STANDARD_VIEW (details_view)->name_renderer),
-                        "xalign", 0.0, "ellipsize", PANGO_ELLIPSIZE_END, "width-chars", 60, NULL);
+                        "xalign", 0.0, "ellipsize", PANGO_ELLIPSIZE_MIDDLE, "width-chars", 60, NULL);
           gtk_tree_view_column_pack_start (details_view->columns[column], THUNAR_STANDARD_VIEW (details_view)->name_renderer, TRUE);
           gtk_tree_view_column_set_attributes (details_view->columns[column], THUNAR_STANDARD_VIEW (details_view)->name_renderer,
                                                "text", THUNAR_COLUMN_NAME,
