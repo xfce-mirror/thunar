cb675c541218c12e57c7ac9d48dfc3bc954e6661
h539s <douglas7527@gmail.com>
2026-02-20 09:49:03 -0300

    sidepane keep focus

--- thunar/thunar-shortcuts-view.c
+++ thunar/thunar-shortcuts-view.c
@@ -750,9 +750,6 @@ thunar_shortcuts_view_key_release_event (GtkWidget   *widget,
     case GDK_KEY_KP_Up:
     case GDK_KEY_KP_Down:
       thunar_shortcuts_view_open (view, THUNAR_ACTION_MANAGER_CHANGE_DIRECTORY);
-
-      /* keep focus on us */
-      gtk_widget_grab_focus (widget);
       break;
     }
 
