xfce-mirror/thunar

commit 2a90df42b0ab24bcfd233f8772ffde6bbfb63238

Use original path when moving files from trash (#142)
Files in trash outside of $HOME do use a very special URI (backslashes, encoded symbols). So we need to query the original path to figure out the basename of such files.

Fixes #142

thunar/thunar-application.c
@@ -955,7 +955,29 @@ thunar_application_collect_and_launch (ThunarApplication     *application,
         }
       else
         {
-          base_name = g_file_get_basename (lp->data);
+          if (thunar_g_file_is_trashed (lp->data))
+            {
+              g_autoptr (GFileInfo) info = NULL;
+
+              /* files in trash location from outside $HOME can have backslashes and doublke-escaping */
+              /* So let's use the original path to obtain a valid basename */
+              info = g_file_query_info (lp->data, G_FILE_ATTRIBUTE_TRASH_ORIG_PATH, G_FILE_QUERY_INFO_NONE, NULL, NULL);
+              if (info != NULL)
+                {
+                  const gchar *original_path = g_file_info_get_attribute_byte_string (info, G_FILE_ATTRIBUTE_TRASH_ORIG_PATH);
+                  base_name = g_path_get_basename (original_path);
+                }
+              else
+                {
+                  base_name = g_file_get_basename (lp->data);
+                  g_warning ("Failed to read basename of trashed file .. falling back to trashed name");
+                }
+            }
+          else
+            {
+              base_name = g_file_get_basename (lp->data);
+            }
+
           file = g_file_resolve_relative_path (target_file, base_name);
           g_free (base_name);
 
@@ -2467,6 +2489,8 @@ thunar_application_move_into (ThunarApplication     *application,
   _thunar_return_if_fail (THUNAR_IS_APPLICATION (application));
   _thunar_return_if_fail (target_file != NULL);
 
+  printf ("thunar_application_move_into\n");
+
   /* Check move validity and get ThunarFile objects */
   for (GList *lp = source_file_list; lp != NULL; lp = lp->next)
     {
@@ -2549,6 +2573,7 @@ thunar_application_move_files (ThunarApplication     *application,
   _thunar_return_if_fail (parent == NULL || GDK_IS_SCREEN (parent) || GTK_IS_WIDGET (parent));
   _thunar_return_if_fail (THUNAR_IS_APPLICATION (application));
 
+  printf ("thunar_application_move_files\n");
   thunar_application_launch (application, parent,
                              "stock_folder-move", _("Moving files ..."),
                              thunar_io_jobs_move_files,
@@ -2974,9 +2999,13 @@ thunar_application_restore_files (ThunarApplication *application,
   _thunar_return_if_fail (parent == NULL || GDK_IS_SCREEN (parent) || GTK_IS_WIDGET (parent));
   _thunar_return_if_fail (THUNAR_IS_APPLICATION (application));
 
+  printf ("thunar_application_restore_files\n");
+
   for (lp = trash_file_list; lp != NULL; lp = lp->next)
     {
+      printf ("lp->data: %s\n", g_file_get_uri (thunar_file_get_file (lp->data)));
       original_uri = thunar_file_get_original_path (lp->data);
+
       if (G_UNLIKELY (original_uri == NULL))
         {
           /* no OriginalPath, impossible to continue */
@@ -2994,6 +3023,9 @@ thunar_application_restore_files (ThunarApplication *application,
 
       /* TODO we might have to distinguish between URIs and paths here */
       target_path = g_file_new_for_commandline_arg (original_uri);
+
+      printf ("original path: %s\n", original_uri);
+      printf ("target_path: %s\n", g_file_get_uri (target_path));
 
       source_path_list = thunar_g_list_append_deep (source_path_list, thunar_file_get_file (lp->data));
       target_path_list = thunar_g_list_append_deep (target_path_list, target_path);

commit 4b9fb8612e819469112abcdcda990a7fdb122119

Remove some debug printfs
Fixes: 2a90df42

thunar/thunar-application.c
@@ -2489,8 +2489,6 @@ thunar_application_move_into (ThunarApplication     *application,
   _thunar_return_if_fail (THUNAR_IS_APPLICATION (application));
   _thunar_return_if_fail (target_file != NULL);
 
-  printf ("thunar_application_move_into\n");
-
   /* Check move validity and get ThunarFile objects */
   for (GList *lp = source_file_list; lp != NULL; lp = lp->next)
     {
@@ -2573,7 +2571,6 @@ thunar_application_move_files (ThunarApplication     *application,
   _thunar_return_if_fail (parent == NULL || GDK_IS_SCREEN (parent) || GTK_IS_WIDGET (parent));
   _thunar_return_if_fail (THUNAR_IS_APPLICATION (application));
 
-  printf ("thunar_application_move_files\n");
   thunar_application_launch (application, parent,
                              "stock_folder-move", _("Moving files ..."),
                              thunar_io_jobs_move_files,
@@ -2999,11 +2996,8 @@ thunar_application_restore_files (ThunarApplication *application,
   _thunar_return_if_fail (parent == NULL || GDK_IS_SCREEN (parent) || GTK_IS_WIDGET (parent));
   _thunar_return_if_fail (THUNAR_IS_APPLICATION (application));
 
-  printf ("thunar_application_restore_files\n");
-
   for (lp = trash_file_list; lp != NULL; lp = lp->next)
     {
-      printf ("lp->data: %s\n", g_file_get_uri (thunar_file_get_file (lp->data)));
       original_uri = thunar_file_get_original_path (lp->data);
 
       if (G_UNLIKELY (original_uri == NULL))
@@ -3023,9 +3017,6 @@ thunar_application_restore_files (ThunarApplication *application,
 
       /* TODO we might have to distinguish between URIs and paths here */
       target_path = g_file_new_for_commandline_arg (original_uri);
-
-      printf ("original path: %s\n", original_uri);
-      printf ("target_path: %s\n", g_file_get_uri (target_path));
 
       source_path_list = thunar_g_list_append_deep (source_path_list, thunar_file_get_file (lp->data));
       target_path_list = thunar_g_list_append_deep (target_path_list, target_path);